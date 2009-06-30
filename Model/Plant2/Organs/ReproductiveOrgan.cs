5f05aac935f92923e5e45adc5049e793ec5471c2
hol353 <dean.holzworth@csiro.au>
2009-06-30 02:05:02 +0000

    Refactored Plant2 ReproductiveOrgan + done some UML diagrams for PLANT2 journal paper.

--- Model/Plant2/Organs/ReproductiveOrgan.cs
+++ Model/Plant2/Organs/ReproductiveOrgan.cs
@@ -4,17 +4,13 @@ using System.Text;
 
 class ReproductiveOrgan : BaseOrgan, Reproductive, AboveGround
    {
-   #region Class Data Members
-   [Event]  public event NullTypeDelegate Harvesting;
+   private bool _ReadyForHarvest = false;
    [Input]  private int Day = 0;
    [Input]  private int Year = 0;
-   [Output] [Units("/m^2")]  private double Number = 0;
    [Param]  private double MaximumSize = 0;
    [Param]  private string RipeStage = "";
-   private bool _ReadyForHarvest = false;
-   #endregion
-
-   [Output] [Units("g/m^2")] double LiveFWt
+   [Output] [Units("/m^2")]  public double Number = 0;
+   [Output] [Units("g/m^2")] public double LiveFWt
       {
       get
          {
@@ -25,7 +21,68 @@ class ReproductiveOrgan : BaseOrgan, Reproductive, AboveGround
             return 0.0;
          }
       }
-   public override void DoActualGrowth()
+   [Output] [Units("g")]     private double Size
+      {
+      get
+         {
+         if (Number > 0)
+            return Live.Wt / Number;
+         else
+            return 0;
+         }
+      }
+   [Output] [Units("g")]     private double FSize
+      {
+      get
+         {
+         if (Number > 0)
+            {
+            Function WC = Children["WaterContent"] as Function;
+            if (WC != null)
+               return (Live.Wt / Number) / (1 - WC.Value);
+            else
+               return 0.0;
+            }
+         else
+            return 0;
+         }
+      }
+   [Output]                  private int ReadyForHarvest
+      {
+      get
+         {
+         if (_ReadyForHarvest)
+            return 1;
+         else
+            return 0;
+         }
+      }
+   [Event]        public event NullTypeDelegate Harvesting;
+   [EventHandler] private void OnHarvest()
+      {
+      Harvesting.Invoke();
+
+      DateTime Today = new DateTime(Year, 1, 1);
+      Today = Today.AddDays(Day - 1);
+      string Indent = "     ";
+      string Title = Indent + Today.ToShortDateString() + "  - Harvesting " + Name + " from " + Plant.Name;
+      double YieldDW = (Live.Wt + Dead.Wt);
+
+      Console.WriteLine("");
+      Console.WriteLine(Title);
+      Console.WriteLine(Indent + new string('-', Title.Length));
+      Console.WriteLine(Indent + Name + " Yield DWt: " + YieldDW.ToString("f2") + " (g/m^2)");
+      Console.WriteLine(Indent + Name + " Size: " + Size.ToString("f2") + " (g)");
+      Console.WriteLine(Indent + Name + " Number: " + Number.ToString("f2") + " (/m^2)");
+      Console.WriteLine("");
+
+
+      Live.Clear();
+      Dead.Clear();
+      Number = 0;
+      _ReadyForHarvest = false;
+      }
+   public override void   DoActualGrowth()
       {
       base.DoActualGrowth();
       if (Plant.Phenology.OnDayOf(RipeStage))
@@ -62,68 +119,5 @@ class ReproductiveOrgan : BaseOrgan, Reproductive, AboveGround
          }
       }
    public override double DMAllocation { set { Live.StructuralWt += value; } }
-   [Output]
-   [Units("g")]
-   private double Size
-      {
-      get
-         {
-         if (Number > 0)
-            return Live.Wt / Number;
-         else
-            return 0;
-         }
-      }
-   [EventHandler]
-   private void OnHarvest()
-      {
-      Harvesting.Invoke();
-
-      DateTime Today = new DateTime(Year, 1, 1);
-      Today = Today.AddDays(Day - 1);
-      string Indent = "     ";
-      string Title = Indent + Today.ToShortDateString() + "  - Harvesting " + Name + " from " + Plant.Name;
-      double YieldDW = (Live.Wt + Dead.Wt);
-
-      Console.WriteLine("");
-      Console.WriteLine(Title);
-      Console.WriteLine(Indent + new string('-', Title.Length));
-      Console.WriteLine(Indent + Name + " Yield DWt: " + YieldDW.ToString("f2") + " (g/m^2)");
-      Console.WriteLine(Indent + Name + " Size: " + Size.ToString("f2") + " (g)");
-      Console.WriteLine(Indent + Name + " Number: " + Number.ToString("f2") + " (/m^2)");
-      Console.WriteLine("");
-
-
-      Live.Clear();
-      Dead.Clear();
-      Number = 0;
-      _ReadyForHarvest = false;
-      }
-   [Output] private int ReadyForHarvest
-      {
-      get
-         {
-         if (_ReadyForHarvest)
-            return 1;
-         else
-            return 0;
-         }
-      }
-   [Output][Units("g")] private double FSize
-      {
-      get
-         {
-         if (Number > 0)
-            {
-            Function WC = Children["WaterContent"] as Function;
-            if (WC != null)
-               return (Live.Wt / Number) / (1 - WC.Value);
-            else
-               return 0.0;
-            }
-         else
-            return 0;
-         }
-      }
    }
 
