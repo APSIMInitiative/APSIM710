3af6c26ab7b1bfd68922422161370217d73a1e7d
hol353 <dean.holzworth@csiro.au>
2009-07-28 03:58:18 +0000

    #3: 1. Next iteration of new manager module - cleaner syntax using managerhelpers. 2. Added an Apply2 to Irrigation module.

--- Model/Build/CreateManagerHelper.bat
+++ Model/Build/CreateManagerHelper.bat
@@ -2,8 +2,8 @@
 pushd .
 cd %APSIM%\Model\ManagerHelper
 
-%MODEL%\ProcessDataTypesInterface     %APSIM%\Documentation\ModelInfo\%1.xml          %APSIM%\Model\ManagerHelper\ManagerHelpers.macro
-..\SetXmlValue ManagerHelper.vcproj @Name %1ManagerHelper
-..\Build\MakeProject ManagerHelper
+%APSIM%\Model\ProcessDataTypesInterface     %APSIM%\Documentation\ModelInfo\%1.xml          %APSIM%\Model\ManagerHelper\ManagerHelpers.macro
+%APSIM%\Model\SetXmlValue                    ManagerHelper.vcproj @Name %1ManagerHelper
+call %APSIM%\Model\Build\MakeProject         ManagerHelper
 
 popd
