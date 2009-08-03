8f2ac66e158b42231beb3dc2922ca89250814ee4
hut104 <neil.huth@csiro.au>
2009-08-03 00:01:45 +0000

    Bad stuff fixed wrt manager helper/ProbeDll.bat stuff

--- Model/Build/ProbeDll.bat
+++ Model/Build/ProbeDll.bat
@@ -14,19 +14,4 @@ rem -------------------------------------------------
 rem                            ModelDLL                ModelXML                       ProbeXMLOutput
 %APSIM%\Model\ProbeDLL     %APSIM%\Model\%1        %APSIM%\Model\%2.xml       %APSIM%\Documentation\ModelInfo\%2.xml
 
-rem -------------------------------------------------
-rem Take the Probe XML file and generate some
-rem Manager Helper source code
-rem -------------------------------------------------
-cd %APSIM%\Model\ManagerHelpers
-rem                                                    ProbeXMLOutput                       ManagerHelperMacro
-%APSIM%\Model\ProcessDataTypesInterface     %APSIM%\Documentation\ModelInfo\%2.xml   %APSIM%\Model\ManagerHelpers\ManagerHelpers.macro
-
-rem -------------------------------------------------
-rem Take the ManagerHelperMacro source code and 
-rem compile to a ManagerHelper .dll
-rem -------------------------------------------------
-%APSIM%\Model\SetXMLValue ManagerHelper.vcproj "@Name" %2ManagerHelper
-"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" ManagerHelper.vcproj /build release
-
 popd
