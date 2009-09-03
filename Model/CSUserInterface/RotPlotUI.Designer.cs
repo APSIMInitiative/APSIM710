442ef9af98502a9014ecac89fed9dc3e60413356
briderj <jason.brider@gmail.com>
2009-09-03 02:41:56 +0000

    Added RotPlot - 1st Component for JB

---
+++ Model/CSUserInterface/RotPlotUI.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace CSUserInterface
+{
+    partial class RotPlotUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(20, 19);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(100, 20);
+            this.txtFile.TabIndex = 2;
+            // 
+            // RotPlotUI
+            // 
+            this.Controls.Add(this.txtFile);
+            this.Name = "RotPlotUI";
+            this.Controls.SetChildIndex(this.txtFile, 0);
+            this.Controls.SetChildIndex(this.MyHelpLabel, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+    }
+}
---
+++ Model/CSUserInterface/RotPlotUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSUserInterface
+{
+    public partial class RotPlotUI : Controllers.BaseView
+    {
+        public RotPlotUI()
+        {
+            InitializeComponent();
+        }
+
+        public override void OnRefresh()
+        {
+            base.OnRefresh();
+            txtFile.Text = "working";
+        }
+
+    }
+}
