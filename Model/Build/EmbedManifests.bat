79f10699847aaeacdac975430a61ce860d99504b
briderj <jason.brider@gmail.com>
2010-06-22 06:55:12 +0000

    Updated batch file to include missing dll. If missing iconv.dll, check it's location in the Buildlibraries folder - it has been moved from bin to lib underneath the iconv folder. bugid: 1154

--- Model/Build/EmbedManifests.bat
+++ Model/Build/EmbedManifests.bat
@@ -4,4 +4,5 @@ mt.exe -manifest %APSIM%\Model\Apsim.exe.manifest -outputresource:%APSIM%\Model\
 mt.exe -manifest %APSIM%\Model\ApsimShared.dll.manifest -outputresource:%APSIM%\Model\ApsimShared.dll;2
 mt.exe -manifest %APSIM%\Model\General.dll.manifest -outputresource:%APSIM%\Model\General.dll;2
 mt.exe -manifest %APSIM%\Model\Protocol.dll.manifest -outputresource:%APSIM%\Model\Protocol.dll;2
+mt.exe -manifest %APSIM%\Model\ShellExtensions.dll.manifest -outputresource:%APSIM%\Model\ShellExtensions.dll;2
 mt.exe -manifest %APSIM%\Model\ComponentInterface.dll.manifest -outputresource:%APSIM%\Model\ComponentInterface.dll;2
