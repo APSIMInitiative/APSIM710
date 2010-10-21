422f91802ebaa577144b068643240b3944139eec
zur003 <eric.zurcher@csiro.au>
2010-10-21 03:28:47 +0000

    Try using the correct Program Files directory when looking for gfortran.

--- Model/Build/RunMake.bat
+++ Model/Build/RunMake.bat
@@ -4,7 +4,7 @@ if "%LIBPATH%" == "" (
       set Path=C:\Progra~1\gfortran;C:\Progra~1\LF9556\Bin;c:\Windows\System32
       call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
    ) else (
-      set Path=C:\Progra~1\gfortran;C:\Progra~2\LF9556\Bin;c:\Windows\System32
+      set Path=C:\Progra~2\gfortran;C:\Progra~2\LF9556\Bin;c:\Windows\System32
       call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
    )
    
