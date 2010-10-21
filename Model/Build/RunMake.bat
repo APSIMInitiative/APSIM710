0c8fd3ef008fbe67a0fe155c01552df9b1ba6574
zur003 <eric.zurcher@csiro.au>
2010-10-21 03:57:01 +0000

    Trying to get the paths to gfortran correct.

--- Model/Build/RunMake.bat
+++ Model/Build/RunMake.bat
@@ -1,10 +1,10 @@
 rem @echo off
 if "%LIBPATH%" == "" (
    if "%ProgramFiles(x86)%" == "" (
-      set Path=C:\Progra~1\gfortran;C:\Progra~1\LF9556\Bin;c:\Windows\System32
+      set Path=C:\Progra~1\gfortran\libexec\gcc\i586-pc-mingw32\4.5.0;C:\Progra~1\gfortran\bin;C:\Progra~1\LF9556\Bin;c:\Windows\System32
       call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
    ) else (
-      set Path=C:\Progra~2\gfortran;C:\Progra~2\LF9556\Bin;c:\Windows\System32
+      set Path=C:\Progra~2\gfortran\libexec\gcc\i586-pc-mingw32\4.5.0;C:\Progra~2\gfortran\bin;C:\Progra~2\LF9556\Bin;c:\Windows\System32
       call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
    )
    
