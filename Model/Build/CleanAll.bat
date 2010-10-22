167336f394cc3cf17f733a9abb85b8cf8ce8e0c9
zur003 <eric.zurcher@csiro.au>
2010-10-22 03:25:21 +0000

    Ensure the *.o (rather than *.obj) files generated by gfortran are removed during cleanup.

--- Model/Build/CleanAll.bat
+++ Model/Build/CleanAll.bat
@@ -5,11 +5,12 @@ rem -------------------------------------------------------------
 pushd ..
 del /Q /S *.map >nul 2>nul
 del /Q /S *.obj >nul 2>nul
+del /Q /S *.o   >nul 2>nul
 del /Q /S *.lib >nul 2>nul
 del /Q /S *.mod >nul 2>nul
 del /Q /S *.bak >nul 2>nul
 del /Q /S *.dsk >nul 2>nul
-del /Q /S *.~* >nul 2>nul
+del /Q /S *.~*  >nul 2>nul
 
 del /Q *.dll >nul 2>nul
 del /Q *.exe >nul 2>nul
