7b520cb9ab8c71b22163d2cad81740880256e964
edmar <edmar.teixeira@plantandfood.co.nz>
2010-11-29 04:21:28 +0000

    Include SoilTemperatureFunction in Plant2 Delete ExpressionEvaluator folder from svn

--- Model/Plant2/Functions/SoilTemperatureFunction.cs
+++ Model/Plant2/Functions/SoilTemperatureFunction.cs
@@ -2,32 +2,82 @@
 using System.Collections.Generic;
 using System.Text;
 using CSGeneral;
-
+/// <summary>
+/// Purpose: Return soil temperature (oC) from a specified soil profile layer.
+/// The source of soil temperature array can be either SoilN ("st" property) or SoilTemp ("ave_soil_temp" property)
+/// </summary>
 class SoilTemperatureFunction : Function
 {
+    
     [Param]
+    [Units("mm")]
     private double Depth = 0;
-    [Input]
+    [Input(Optional = true)]
+    [Units("oC")]
     private double[] ave_soil_temp = null;
+    [Input(Optional = true)]
+    [Units("oC")]
+    private double[] st = null;
     [Input]
+    [Units("mm")]
     public double[] dlayer = null; // Depth of each soil layer in mm         
 
+    string soilTempSource = "Unknown"; //Flag for the name of soil temperature array
+
     [Output]
+    [Units("oC")]
     public override double Value
     {
         get
         {
-            double cummDepth = 0.0; // Cummulative depth
-            for (int i = 0; i < dlayer.Length; i++)
-            { 
-                cummDepth += dlayer[i];
-                if (cummDepth >= Depth)
+            int Layer = LayerIndex(Depth, dlayer);
+
+            if (soilTempSource == "Unknown")
+            {
+                if (ave_soil_temp != null)
                 {
-                    return ave_soil_temp[i];                  
+                    soilTempSource = "ave_soil_temp";
+                }            
+                else if  (st != null)
+                {
+                    soilTempSource = "st";
                 }
+                else
+                {
+                    throw new Exception(Name + ": Soil temperature was not found "); 
+                
+                } 
             }
-            throw new Exception("SoilTemperatureFunction: Specified soil depth of " + Depth.ToString() + " mm is greater than profile depth of " + cummDepth.ToString() + " mm"); 
+
+            switch (soilTempSource)
+            {
+                case "ave_soil_temp":
+                    return ave_soil_temp[Layer];
+                case "st":
+                    return st[Layer];
+                default:
+                    throw new Exception(Name + ": Unknown soil temperature source: " + soilTempSource);
+           }
+                 
         }
     }
+    /// <summary>
+    /// Returns the soil layer index for a specified soil depth (mm)
+    /// </summary>
+    /// <param name="depth">Soil depth (mm)</param>
+    /// <param name="dlayer">Array of soil layer depths in the profile (mm)</param>
+    /// <returns>soil layer index</returns>
+    private int LayerIndex(double depth, double[] dlayer)
+    {
+        double CumDepth = 0.0;
+        for (int i = 0; i < dlayer.Length; i++)
+        {
+            CumDepth = CumDepth + dlayer[i];
+            if (CumDepth >= depth) { return i; }
+        }
+        throw new Exception(Name + ": Specified soil depth of " + Depth.ToString() + " mm is greater than profile depth of " + CumDepth.ToString() + " mm"); 
+    }
+
+
 
 }
