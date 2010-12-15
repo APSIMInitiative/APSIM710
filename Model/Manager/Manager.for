d89ee961fff21bf6635312393166e1e25032abf0
zur003 <eric.zurcher@csiro.au>
2010-12-15 05:45:46 +0000

    Try again...

--- Model/Manager/Manager.for
+++ Model/Manager/Manager.for
@@ -44,7 +44,7 @@ C     Last change:  P    25 Oct 2000    9:26 am
       parameter (Max_variable_value_size=100)
 
       integer Max_tokens               ! Maximum number of tokens
-      parameter (Max_tokens=5000)  ! 10000 !!!
+      parameter (Max_tokens=8000)  ! 10000 !!!
 
       integer Max_token_size           ! Maximum size of a token
       parameter (Max_token_size=700)
