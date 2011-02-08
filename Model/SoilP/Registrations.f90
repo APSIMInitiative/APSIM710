e5a71273542913ee654f798ab905f2d1271b2bcc
devoilp <p.devoil@uq.edu.au>
2011-02-08 22:57:38 +0000

    Descriptions for soil P variables bugid: 1034

--- Model/SoilP/Registrations.f90
+++ Model/SoilP/Registrations.f90
@@ -21,6 +21,7 @@ module Registrations
       subroutine doRegistrations(id)
          use Infrastructure
          type(IDsType) :: id
+         integer dummy
 
          id%externalmassflow = add_registration(eventReg, 'externalmassflow', ExternalMassFlowTypeDDML, '')
          id%sysinit = add_registration(respondToEventReg, 'sysinit', nullTypeDDML, '')
@@ -33,6 +34,22 @@ module Registrations
          id%actualresiduedecompositioncalculated = add_registration(respondToEventReg, 'actualresiduedecompositioncalculated', SurfaceOrganicMatterDecompTypeDDML, '')
          id%tick = add_registration(respondToEventReg, 'tick', nullTypeDDML, '')
          id%process = add_registration(respondToEventReg, 'process', nullTypeDDML, '')
+
+         dummy = add_reg(respondToGetSetReg, 'labile_p', floatarrayTypeDDML, 'kg/ha', 'Labile P')
+         dummy = add_reg(respondToGetReg, 'unavail_p', floatarrayTypeDDML, 'kg/ha', 'Unavailable P')
+         dummy = add_reg(respondToGetSetReg, 'banded_p', floatarrayTypeDDML, 'kg/ha', 'Banded P')
+         dummy = add_reg(respondToGetSetReg, 'rock_p', floatarrayTypeDDML, 'kg/ha', 'Rock P')
+         dummy = add_reg(respondToGetReg, 'soil_test_p', floatarrayTypeDDML, 'kg/ha', 'Labile + Banded P')
+         dummy = add_reg(respondToGetReg, 'biom_p', floatarrayTypeDDML, 'kg/ha', 'P in biomass pool')
+         dummy = add_reg(respondToGetReg, 'humic_p', floatarrayTypeDDML, 'kg/ha', 'P in humic pool')
+         dummy = add_reg(respondToGetReg, 'fom_p', floatarrayTypeDDML, 'kg/ha', 'P in FOM pool')
+         dummy = add_reg(respondToGetReg, 'fom_p_pool1', floatarrayTypeDDML, 'kg/ha', 'P in FOM pool 1')
+         dummy = add_reg(respondToGetReg, 'fom_p_pool2', floatarrayTypeDDML, 'kg/ha', 'P in FOM pool 2')
+         dummy = add_reg(respondToGetReg, 'fom_p_pool3', floatarrayTypeDDML, 'kg/ha', 'P in FOM pool 3')
+         dummy = add_reg(respondToGetReg, 'fom_cp', floatarrayTypeDDML, 'kg/ha', 'C:P ratio in FOM pool')
+         dummy = add_reg(respondToGetReg, 'uptake_p_{crop}', floatarrayTypeDDML, 'kg/ha', 'P uptake from {crop}')
+         dummy = add_reg(respondToGetReg, 'demand_p_{crop}', floatTypeDDML, 'kg/ha', 'P demand from {crop}')
+
       end subroutine
 end module Registrations
 
