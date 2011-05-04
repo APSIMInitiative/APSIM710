bfd63027b93624aeffcf1cb9a39f12075ecf16e2
her123 <neville.herrmann@csiro.au>
2011-05-04 03:15:04 +0000

    Conditional use of ModelFramework

--- Model/Plant2/Functions/OnEventFunction.cs
+++ Model/Plant2/Functions/OnEventFunction.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ModelFramework;
-
+#if !fulldotnet  
+    using ModelFramework;
+#endif
 
 class OnEventFunction : Function
    {
