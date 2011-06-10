b1f9f0711051fd4a706cd2ed73caa446220f36a4
hol353 <dean.holzworth@csiro.au>
2011-06-10 04:14:05 +0000

    Whoops forgot these 2 unit test file modifications.

--- Model/Build/RemoveUnitTestTimeStamp.tcl
+++ Model/Build/RemoveUnitTestTimeStamp.tcl
@@ -3,11 +3,11 @@
 catch {set msg [exec svn log -q -r HEAD ./]} msg
 set msg [lindex [split $msg "\n"] 1]
 
-set fp [open ../../Tests/UnitTests/UnitTests.out r]; set text [read -nonewline $fp]; close $fp
+set fp [open UnitTest.out r]; set text [read -nonewline $fp]; close $fp
 
 regsub -all -line ", Time: .* seconds$" $text "" text
 
-set fp [open ../../Tests/UnitTests/UnitTests.out w]
+set fp [open UnitTest.out w]
 puts -nonewline $fp $text
 close $fp
 
--- Model/Build/RunAllUnitTests.bat
+++ Model/Build/RunAllUnitTests.bat
@@ -6,42 +6,25 @@ pushd ..\..
 set APSIM=%CD%
 
 cd %APSIM%\Tests\UnitTests
-del /Q UnitTests.out 2>nul
 
 rem -------------------------------------------------------------
 rem Go find all unit tests and execute them.
 rem -------------------------------------------------------------
 for /D %%d in (*) do (
    cd %APSIM%\Tests\UnitTests\%%d
-   echo ----------------------------------------------------- >> ..\UnitTests.out
-   echo %%d >> ..\UnitTests.out
-   echo ----------------------------------------------------- >> ..\UnitTests.out
-   echo -----------------------------------------------------
-   echo %%d
-   echo -----------------------------------------------------
    if EXIST Test.sln (
-      "%VS100COMNTOOLS%..\IDE\devenv" Test.sln /ReBuild debug
-      if ERRORLEVEL 1 echo Compile ERRORS FOUND >> ..\UnitTests.out
+      "%VS100COMNTOOLS%..\IDE\devenv" Test.sln /ReBuild debug > UnitTest.out
       if ERRORLEVEL 0 (
-         cmd /c "C:\Program Files (x86)\NUnit 2.5.7\bin\net-2.0\nunit-console-x86" bin\debug\Test.dll /nologo /noshadow >> ..\UnitTests.out
-         if ERRORLEVEL 1 echo ERRORS FOUND >> ..\UnitTests.out
+         "C:\Program Files (x86)\NUnit 2.5.7\bin\net-2.0\nunit-console-x86" bin\debug\Test.dll /nologo /noshadow >> UnitTest.out
          )
-      echo. >> ..\UnitTests.out
-      echo. >> ..\UnitTests.out
       )
    if EXIST Makefile (
-      call %APSIM%\Model\Build\RunMake -B
-      if ERRORLEVEL 1 echo ERRORS FOUND >> ..\UnitTests.out
-      if ERRORLEVEL 0 (
-         %APSIM%\Model\Test.exe >> ..\UnitTests.out
-         if ERRORLEVEL 1 echo ERRORS FOUND >> ..\UnitTests.out
-         )
+      call %APSIM%\Model\Build\RunMake -B > UnitTest.out
+      if ERRORLEVEL 0 %APSIM%\Model\Test.exe >> UnitTest.out
       del /Q %APSIM%\Model\Test.* 2>nul
-      echo. >> ..\UnitTests.out
-      echo. >> ..\UnitTests.out
       )
+   if EXIST UnitTest.out "%APSIM%\..\BuildLibraries\tcl\ASTcl\bin\tclsh84.exe" %APSIM%\Model\Build\RemoveUnitTestTimeStamp.tcl
    )
 
 popd
-"%APSIM%\..\BuildLibraries\tcl\ASTcl\bin\tclsh84.exe" RemoveUnitTestTimeStamp.tcl
 
