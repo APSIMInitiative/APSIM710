1e17f88fe0fabf58bc2eaf48235644b452696fb7
hol353 <dean.holzworth@csiro.au>
2011-06-14 05:18:05 +0000

    Fixed UnitTests (again)

--- Model/Build/RunAllUnitTests.bat
+++ Model/Build/RunAllUnitTests.bat
@@ -13,14 +13,14 @@ rem -------------------------------------------------------------
 for /D %%d in (*) do (
    cd %APSIM%\Tests\UnitTests\%%d
    if EXIST Test.sln (
-      "%VS100COMNTOOLS%..\IDE\devenv" Test.sln /ReBuild debug > UnitTest.out
+      "%VS100COMNTOOLS%..\IDE\devenv" Test.sln /ReBuild debug
       if ERRORLEVEL 0 (
-         "C:\Program Files (x86)\NUnit 2.5.7\bin\net-2.0\nunit-console-x86" bin\debug\Test.dll /nologo /noshadow >> UnitTest.out
+         "C:\Program Files (x86)\NUnit 2.5.7\bin\net-2.0\nunit-console-x86" bin\debug\Test.dll /nologo /noshadow > UnitTest.out
          )
       )
    if EXIST Makefile (
-      call %APSIM%\Model\Build\RunMake -B > UnitTest.out
-      if ERRORLEVEL 0 %APSIM%\Model\Test.exe >> UnitTest.out
+      call %APSIM%\Model\Build\RunMake -B
+      if ERRORLEVEL 0 %APSIM%\Model\Test.exe > UnitTest.out
       del /Q %APSIM%\Model\Test.* 2>nul
       )
    if EXIST UnitTest.out "%APSIM%\..\BuildLibraries\tcl\ASTcl\bin\tclsh84.exe" %APSIM%\Model\Build\RemoveUnitTestTimeStamp.tcl
