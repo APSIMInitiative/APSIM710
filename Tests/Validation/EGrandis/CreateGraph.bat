c59c92b36916bd3d33619515c6c367d61d4ae8be
fai04d <justin.fainges@csiro.au>
2011-06-21 02:59:12 +0000

    EGrandis fixes and clean up.

--- Tests/Validation/EGrandis/CreateGraph.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\ApsimReport\ApsimReport.exe" CoffsHarbour.report %APSIM%\Graphs\EGrandis\Validation\CoffsHarbour.gif
-"C:\Program Files (x86)\ApsimReport\ApsimReport.exe" Gympie.report %APSIM%\Graphs\EGrandis\Validation\Gympie.gif
-"C:\Program Files (x86)\ApsimReport\ApsimReport.exe" Wagga.report %APSIM%\Graphs\EGrandis\Validation\Wagga.gif
