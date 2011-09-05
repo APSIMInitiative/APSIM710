f1f402a202f2d73539722793802c83b4711ec947
zur003 <eric.zurcher@csiro.au>
2011-09-05 07:59:01 +0000

    Also fixing type reporting in AgPasture bugid: 1371

---
+++ Model/AgPasture/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AgPasture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AgResearch")]
+[assembly: AssemblyProduct("AgPasture")]
+[assembly: AssemblyCopyright("Copyright © AgResearch 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cbb8763-3625-4a17-bfb7-d4df0e69e883")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.0")]
+//[assembly: AssemblyFileVersion("7.4.0.0")]
