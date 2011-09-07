1af79f26639b34ebd43df5c8fd1832201a9d4bf0
her123 <neville.herrmann@csiro.au>
2011-09-07 12:31:44 +0000

    Corrected type for plant2 in DotNetProxies bugid: 1360

--- Model/Plant2/Properties/AssemblyInfo.cs
+++ Model/Plant2/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plant.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Plant2")]
+[assembly: AssemblyDescription("Plant2 .net component")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CSIRO Sustainable Ecosystems")]
-[assembly: AssemblyProduct("Plant.NET")]
-[assembly: AssemblyCopyright("Copyright © CSIRO Sustainable Ecosystems 2008")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CSIRO Sustainable Ecosystems")] //author
+[assembly: AssemblyProduct("Plant2")]   //class
+[assembly: AssemblyCopyright("Copyright © CSIRO Sustainable Ecosystems 2011")]
+[assembly: AssemblyTrademark("CSIRO")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5c61a6c8-d1da-4b36-946a-5382df8a3b6f")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+//[assembly: AssemblyFileVersionAttribute("1.0.0.1")]
