0bd003992d864f867355c7b33ab3c5ba901cbb98
hol353 <dean.holzworth@csiro.au>
2011-09-12 09:58:32 +0000

    Stop invalid chars from being written to BuildOutput.xml & Bob's details XML file. bugid: 1374

--- Release/Release.bat
+++ Release/Release.bat
@@ -73,19 +73,19 @@ copy ApsimSetupForRelease.exe C:\inetpub\wwwroot\Files\%1.ApsimSetupForRelease.e
 rem Now a self extracting "binaries only" for cluster runs
 set dest=Temp
 mkdir %dest%
-copy  ..\Apsim.xml %dest%\Apsim.xml
+copy  ..\Apsim.xml %dest%\Apsim.xml >nul
 mkdir %dest%\Model
-copy  ..\Model\*.xml %dest%\Model
-copy  ..\Model\*.dll %dest%\Model
-copy  ..\Model\*.exe %dest%\Model
+copy  ..\Model\*.xml %dest%\Model >nul
+copy  ..\Model\*.dll %dest%\Model >nul
+copy  ..\Model\*.exe %dest%\Model >nul
 mkdir %dest%\Model\TclLink
-copy  ..\Model\TclLink\CIDataTypes.tcl %dest%\Model\TclLink
-xcopy /s ..\Model\TclLink\bin %dest%\Model\TclLink\bin
-xcopy /s ..\Model\TclLink\lib %dest%\Model\TclLink\lib
+copy  ..\Model\TclLink\CIDataTypes.tcl %dest%\Model\TclLink >nul
+xcopy /s /i ..\Model\TclLink\bin %dest%\Model\TclLink\bin >nul
+xcopy /s /i ..\Model\TclLink\lib %dest%\Model\TclLink\lib >nul
 mkdir %dest%\UserInterface
-copy ..\UserInterface\*.xml %dest%\UserInterface
+copy ..\UserInterface\*.xml %dest%\UserInterface >nul
 mkdir %dest%\UserInterface\ToolBoxes
-copy ..\UserInterface\ToolBoxes\*.xml %dest%\UserInterface\ToolBoxes
+copy ..\UserInterface\ToolBoxes\*.xml %dest%\UserInterface\ToolBoxes >nul
 
 "c:\Program Files\7-Zip\7z.exe" a -sfx -mx=9 -mmt=on %1.binaries.Win32.exe %dest%
 
