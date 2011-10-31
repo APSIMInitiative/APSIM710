f4cf2c1cf6c8838c6aad8cd877ba1e492c39f28a
zur003 <eric.zurcher@csiro.au>
2011-10-31 16:56:43 +0000

    Delete obsolete EntryPoints and macro files bugid: 1360

--- Model/DataTypes/DataTypes.bat
+++ Model/DataTypes/DataTypes.bat
@@ -1,6 +1,5 @@
 pushd %APSIM%\Model\DataTypes
 %APSIM%\Model\processdatatypesinterface DataTypes.xml CI2Datatypes.macro
-%APSIM%\Model\processdatatypesinterface DataTypes.xml DOTNETDatatypes.macro
 %APSIM%\Model\processdatatypesinterface DataTypes.xml CIDatatypes.macro
 %APSIM%\Model\processdatatypesinterface DataTypes.xml CSDOTNETDataTypes.macro
 %APSIM%\Model\processdatatypesinterface DataTypes.xml CSDataTypes.macro
