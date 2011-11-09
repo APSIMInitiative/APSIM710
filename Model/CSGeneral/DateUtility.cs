5e7e551cb6e250b530b13c93b11c9a7e918ce92a
jol337 <jol337>
2011-11-09 06:19:41 +0000

    Add some comments to DateUtilities bugid: 1400

--- Model/CSGeneral/DateUtility.cs
+++ Model/CSGeneral/DateUtility.cs
@@ -17,6 +17,11 @@ namespace CSGeneral
             rxDD = new Regex(@"\d\d?"),
             rxMMM = new Regex(@"\w{3}");
 
+        /// <summary>
+        /// Convert a Julian Date to a DateTime object
+        /// </summary>
+        /// <param name="julian_date"></param>
+        /// <returns></returns>
         public static DateTime GetDate(double julian_date)
         {
             double a, b, c, d, e, f, z, alpha, decDay;
@@ -62,11 +67,22 @@ namespace CSGeneral
             return new DateTime(yr, mnth, day, hr, min, sec, ms);
         }
 
+        /// <summary>
+        /// Get a DateTime from a ddMMM string (ie '01Jan' OR '1-Jan' OR '1 Jan' etc), year is automatically set to 1900
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <returns>A DateTime with the specified date and month, year = 1900</returns>
         public static DateTime GetDate(string ddMMM)
         {
             return GetDate(ddMMM, 1900);
         }
 
+        /// <summary>
+        /// Get a DateTime from a 'ddMMM' string (ie '01Jan' OR '1-Jan' OR '1 Jan' etc)
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="year">The year to use when constructing the DateTime object</param>
+        /// <returns>A DateTime constructed from <paramref name="ddMMM"/> using <paramref name="year"/></returns>
         public static DateTime GetDate(string ddMMM, int year)
         {
             try
@@ -86,36 +102,77 @@ namespace CSGeneral
             }
         }
 
+        /// <summary>
+        /// Get a DateTime from a 'ddMMM' string (ie '01Jan' OR '1-Jan' OR '1 Jan' etc), using <paramref name="today"/> to get the year to use
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="today">The year in this parameter will be used to construct the result</param>
+        /// <returns>A DateTime constructed from <paramref name="ddMMM"/> using the year of <paramref name="today"/></returns>
         public static DateTime GetDate(string ddMMM, DateTime today)
         {
             return GetDate(ddMMM, today.Year);
         }
 
+        /// <summary>
+        /// Given today's date (<paramref name="today"/>), get the next occurrence of <paramref name="thedate"/> by adding/subtracting year(s)
+        /// </summary>
+        /// <param name="thedate">The date to change</param>
+        /// <param name="today">Today's date</param>
+        /// <returns>The next occurrence of <paramref name="thedate"/></returns>
         public static DateTime GetNextDate(DateTime thedate, DateTime today)
         {
+            thedate = thedate.AddYears(today.Year - thedate.Year);
             return today.CompareTo(thedate) < 0 ? thedate : thedate.AddYears(1);
         }
         
+        /// <summary>
+        /// Given a 'ddMMM' string (ie '01Jan' OR '1-Jan' OR '1 Jan' etc) and <paramref name="today"/>, return the next occurrence of <paramref name="ddMMM"/>
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="today">Today's date</param>
+        /// <returns>The next occurrence of <paramref name="ddMMM"/></returns>
         public static DateTime GetNextDate(string ddMMM, DateTime today)
         {
             return GetNextDate(GetDate(ddMMM, today), today);
         }
 
+        /// <summary>
+        /// Construct a DateTime from <paramref name="ddMMM"/> and <paramref name="today"/> then 'CompareTo' <paramref name="today"/>
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="today">Today's date</param>
+        /// <returns>+1 if <paramref name="ddMMM"/> is less than <paramref name="today"/>, 0 if equal, -1 if greater</returns>
         public static int CompareDates(string ddMMM, DateTime today)
         {
             return today.CompareTo(GetDate(ddMMM, today));
         }
 
+        /// <summary>
+        /// Compare <paramref name="ddMMM"/> and <paramref name="today"/> (ignoring year component)
+        /// </summary>
+        /// <param name="ddMMM">String containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="today">The date to check</param>
+        /// <returns>true if the day and month components of <paramref name="today"/> match ddMMM, else false</returns>
         public static bool DatesEqual(string ddMMM, DateTime today)
         {
-            return CompareDates(ddMMM, today) == 0;
+            return 
+                today.Month == LowerCaseMonths.IndexOf(rxMMM.Match(ddMMM).Value.ToLower())
+                &&
+                today.Day == int.Parse(rxDD.Match(ddMMM).Value);
         }
 
-        public static bool WithinDates(string ddMMM_date1, DateTime today, string ddMMM_date2)
+        /// <summary>
+        /// Look to see if <paramref name="today"/> lies between <paramref name="ddMMM_start"/> and <paramref name="ddMMM_end"/> (will handle year boundaries)
+        /// </summary>
+        /// <param name="ddMMM_start">The start date - a string containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <param name="today">The date to check</param>
+        /// <param name="ddMMM_end">The end date - a string containing 'day of month' and at least the first 3 letters of a month's name</param>
+        /// <returns><paramref name="ddMMM_start"/> &lt;&eq; <paramref name="today"/> &lt;&eq; <paramref name="ddMMM_end"/></returns>
+        public static bool WithinDates(string ddMMM_start, DateTime today, string ddMMM_end)
         {
             DateTime
-                start = GetDate(ddMMM_date1, today.Year),
-                end = GetDate(ddMMM_date2, today.Year);
+                start = GetDate(ddMMM_start, today.Year),
+                end = GetDate(ddMMM_end, today.Year);
 
             //if start after end (could be after if spanning years)
             if (start.CompareTo(end) > 0)
@@ -124,11 +181,23 @@ namespace CSGeneral
             return WithinDates(start, today, end);
         }
 
-        public static bool WithinDates(DateTime date1, DateTime today, DateTime date2)
+        /// <summary>
+        /// Look to see if <paramref name="today"/> lies between <paramref name="start"/> and <paramref name="end"/> (will handle year boundaries)
+        /// </summary>
+        /// <param name="start">The start date</param>
+        /// <param name="today">The date to check</param>
+        /// <param name="end">The end date</param>
+        /// <returns><paramref name="start"/> &lt;&eq; <paramref name="today"/> &lt;&eq; <paramref name="end"/></returns>
+        public static bool WithinDates(DateTime start, DateTime today, DateTime end)
         {
-            return today.CompareTo(date1) >= 0 && today.CompareTo(date2) <= 0;
+            return today.CompareTo(start) >= 0 && today.CompareTo(end) <= 0;
         }
 
+        /// <summary>
+        /// Get a Julian Date from a DateTime
+        /// </summary>
+        /// <param name="date">The DateTime to convert</param>
+        /// <returns>The Julian Date representation of <paramref name="date"/></returns>
         public static double GetJulianDate(DateTime date)
         {
             double yr;
