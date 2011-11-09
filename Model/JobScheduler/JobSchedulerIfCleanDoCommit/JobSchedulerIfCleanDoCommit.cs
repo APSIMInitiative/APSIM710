c0682abe5298c3f9241b8c262b260672b80b62a4
jol337 <jol337>
2011-11-09 08:25:42 +0000

    Undo my changes as they are obviously not working (adding revision to Bob binaries) bugid: 1404

--- Model/JobScheduler/JobSchedulerIfCleanDoCommit/JobSchedulerIfCleanDoCommit.cs
+++ Model/JobScheduler/JobSchedulerIfCleanDoCommit/JobSchedulerIfCleanDoCommit.cs
@@ -5,7 +5,6 @@ using System.Text;
 using CSGeneral;
 using System.Diagnostics;
 using System.IO;
-using System.Xml;
 
 
 class JobSchedulerIfCleanDoCommit
@@ -36,21 +35,8 @@ class JobSchedulerIfCleanDoCommit
         DB.Open();
         int JobID = Convert.ToInt32(JobScheduler.TalkToJobScheduler("GetVariable~JobID"));
 
-        int revision = -1;
         try
         {
-            string SVNFileName = Utility.FindFileOnPath("svn.exe");
-            if (SVNFileName == "")
-                throw new Exception("Cannot find svn.exe on PATH");
-
-            //Console.WriteLine("XML Version Number START");
-            XmlDocument doc = new XmlDocument();
-            //string str = Utility.CheckProcessExitedProperly(Utility.RunProcess(SVNFileName, "info --xml", ApsimDirectory));
-            // Console.WriteLine(str);
-            doc.LoadXml(Utility.CheckProcessExitedProperly(Utility.RunProcess(SVNFileName, "info --xml", ApsimDirectory)));
-            int.TryParse(XmlHelper.Attribute(XmlHelper.FindRecursively(doc.FirstChild.NextSibling, "entry"), "revision"), out revision);
-            //Console.WriteLine("XML Version Number = " + revision.ToString());
-
             Dictionary<string, object> Details = DB.GetDetails(JobID);
 
             string Status = Details["Status"].ToString();
@@ -59,6 +45,10 @@ class JobSchedulerIfCleanDoCommit
 
             else if (Status == "Pass")
             {
+                string SVNFileName = Utility.FindFileOnPath("svn.exe");
+                if (SVNFileName == "")
+                    throw new Exception("Cannot find svn.exe on PATH");
+
                 string BugID = Details["BugID"].ToString();
                 string Description = Details["Description"].ToString() + "\r\nbugid: " + BugID;
                 string SVNArguments = "commit --username " + Details["UserName"] + " --password " + Details["Password"] +
@@ -66,18 +56,12 @@ class JobSchedulerIfCleanDoCommit
                 Process SVNP = Utility.RunProcess(SVNFileName, SVNArguments, ApsimDirectory);
                 string StdOut = Utility.CheckProcessExitedProperly(SVNP);
                 Console.WriteLine(StdOut);
-
-                Environment.SetEnvironmentVariable("Revision", "r" + (revision + 1).ToString(), EnvironmentVariableTarget.Machine);
             }
             else
-            {
-                Environment.SetEnvironmentVariable("Revision", "r" + revision.ToString() + "FAILED", EnvironmentVariableTarget.Machine);
                 Console.WriteLine("Not clean - no commit");
-            }
         }
         catch (Exception err)
         {
-            Environment.SetEnvironmentVariable("Revision", "r" + revision.ToString() + "ERR" + err.Message, EnvironmentVariableTarget.Machine);
             DB.UpdateStatus(JobID, "Fail");
             DB.Close();
             throw err;
