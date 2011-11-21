d5a2b75d74e4d3dff44e8206308747757b565f89
hamish <hamish.brown@plantandfood.co.nz>
2011-11-21 05:42:16 +0000

    Progress with Oat2 validation bugid: 1015

--- Model/Plant2/Phenology/FacultativeVernalisationPhase.cs
+++ Model/Plant2/Phenology/FacultativeVernalisationPhase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-public class ObligateVernalisationPhase : Phase
+public class FacultativeVernalisationPhase : Phase
 {
     [Link]
     Function JuvenileDevelopmentIndex = null;
