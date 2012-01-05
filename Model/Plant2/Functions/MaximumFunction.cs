3303f64a5a3470c77c78da9f6714cc4377e83b95
hamish <hamish.brown@plantandfood.co.nz>
2012-01-05 05:15:21 +0000

    Adding maximum function. Deleting LeafStagePhylochronFunction (not needed) bugid: 1015

---
+++ Model/Plant2/Functions/MaximumFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using CSGeneral;
+
+class MaximumFunction : Function
+   {
+   [Output]
+   public override double Value
+      {
+      get
+         {
+         double ReturnValue = 999999999;
+         foreach (Function F in My.ChildrenAsObjects)
+            {
+                ReturnValue = Math.Max(ReturnValue, F.Value);
+            }
+         return ReturnValue;
+         }
+      }
+
+   }
