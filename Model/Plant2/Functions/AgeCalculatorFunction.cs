c92f0dc9a8b581b028daa532e5f159746c06ffa7
hut104 <neil.huth@csiro.au>
2012-01-31 10:21:26 +0000

    Prototype Oil Palm Model bugid: 1205

---
+++ Model/Plant2/Functions/AgeCalculatorFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using CSGeneral;
+
+class AgeCalculatorFunction : Function
+{
+    private int _Age = 0;
+
+    [EventHandler]
+    private void OnTick(TimeType T)
+    {
+        _Age = _Age + 1;
+    }
+    [Output]
+    [Units("y")]
+    public override double Value
+    {
+        get
+        {
+            return _Age/365.25;
+        }
+    }
+    [Output("Age")]
+    [Units("y")]
+    public double Age
+    {
+        get
+        {
+            return _Age / 365.25;
+        }
+    }
+
+}
