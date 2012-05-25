8236b2024ba16e101bdfe970d165fba700e0a268
jonas.vejlin <jonas.vejlin>
2012-05-25 11:45:58 +0000

    same as before bugid: 1515

--- Model/SlurryTank/SlurryTank/slurryTank.cs
+++ Model/SlurryTank/SlurryTank/slurryTank.cs
@@ -116,12 +116,12 @@ using System.Collections.Generic;
         [EventHandler]
         public void OnInitialised()
         {
-            Console.Out.WriteLine("Entered OnInitialised in Slurry Tank");
+
         }
         [EventHandler]
         public void OnPrepare()
         {
-            Console.Out.WriteLine("Entered OnPrepare in Slurry Tank");
+
         }
 
         private double GetOM()
@@ -139,11 +139,6 @@ using System.Collections.Generic;
         [EventHandler]
         public void OnAddSlurry(ManureType input)
         {
-            Console.WriteLine(TankName);
-            Console.WriteLine(input.WhereTo);
-            if (input.WhereTo.Equals(TankName))
-            {
-
                 slurrypH = input.pH;
                 double amount = input.amount;
                 double DM = amount * input.DM;
@@ -163,13 +158,13 @@ using System.Collections.Generic;
                 HInert += 0.055 * C_InertAdded;//1.34
                 OInert += 0.444 * C_InertAdded;// 1.35
                 //! Calculate the carbon content of the raw protein
-                CRP += 4.28 * NFast; //1.6 //kg
+                CRP = 4.28 * (1-input.fInert)*orgNAdded; //1.6 //kg
                 //! Calculate the carbon content of the lignin
-                CLignin += DM * 0.55 * input.ADL; //1.8 //kg
+                CLignin = DM * 0.55 * input.ADL; //1.8 //kg
                 //! Calculate the carbon content of the slow pool
                 CSlow += DM * 0.44 * (input.NDF - input.ADL); //1.9 //kg
                 //! Calculate the carbon content of the raw lipid
-                CRL += DM * 0.77 * input.RL; //1.10 //kg
+                CRL = DM * 0.77 * input.RL; //1.10 //kg
                 //! calculate the carbon content of the volatile fatty acids
                 CVFA += DM * 0.4 * input.VFA; //1.11 //kg
                 HVFA += DM * 0.167 * input.VFA;//1.16 // kg
@@ -177,180 +172,202 @@ using System.Collections.Generic;
                 //! Calculate the carbon content of the starch and sugar
                 double CStarch = DM * 0.44 * input.Rem; //1.12 //kg
                 //! Calculate the carbon content of the Fast pool
-                CFast = CRP + CRL + CStarch; //1.13
+                CFast += CRP + CRL + CStarch; //1.13
                 //! Calculate the hydrogen in the Fast and Slow pools
-                HFast = 0.117 * CRP + 0.152 * CRL + 0.139 * CStarch;//1.14
-                HSlow = 0.139 * CSlow;//1.15
+                HFast += 0.117 * CRP + 0.152 * CRL + 0.139 * CStarch;//1.14
+                HSlow += 0.139 * CSlow;//1.15
                 //! Calculate the oxygen in the Fast and Slow
-                OFast = 0.533 * CRP + 0.14 * CRL + 1.111 * CStarch;//1.17
-                OSlow = 1.111 * CSlow;//1.18
+                OFast += 0.533 * CRP + 0.14 * CRL + 1.111 * CStarch;//1.17
+                OSlow += 1.111 * CSlow;//1.18
                 //! Calculate the sulphur in the Fast and Sulphate pools
                 SFast += amount * (input.TotalS - (input.SulphS + input.SulphS));//1.20
                 S_S04 += amount * input.TotalS;//1.21
                 S2_S += amount * input.TotalS; // 1.22
                 Water += amount * (1 - input.DM); // 1.1 //kg
 
-            }
-            Console.Out.WriteLine("Leaving OnAddSlurry in Slurry Tank");
              
         }
 
         [EventHandler]
         public void OnProcess()
         {
-           
-            Console.Out.WriteLine("Entered OnProcess in Slurry Tank");
-            temperatureInKelvin = 283.0;
-            double temperatureInCelsius = temperatureInKelvin - 273.15;
-	//! Calculate the normalised temperature effect
-	double FTheta=Math.Pow(E,ThetaA+ThetaB*temperatureInCelsius *(1-0.5*(temperatureInCelsius /ThetaC)));  //1.26
-	double FpH=1.0; //need to implement this
+            if (Water != 0)
+            {
 
-	//! Calculate the degradation rates of the Fast and Slow pools
-	double k1act=FpH*FTheta*k1;//1.25 //should be 0 to 1
-	double k2act=FpH*FTheta*k2;//1.25
+      
+                temperatureInKelvin = 283.0;
+                double temperatureInCelsius = temperatureInKelvin - 273.15;
+                //! Calculate the normalised temperature effect
+                double FTheta = Math.Pow(E, ThetaA + ThetaB * temperatureInCelsius * (1 - 0.5 * (temperatureInCelsius / ThetaC)));  //1.26
+                double FpH = 1.0; //need to implement this
 
-	double hydrolysedCpool =k1act*CSlow+k2act*CFast;
-	double hydrolysedHpool =k1act*HSlow+k2act*HFast;
-	double hydrolysedOpool =k1act*OSlow+k2act*OFast;
+                //! Calculate the degradation rates of the Fast and Slow pools
+                double k1act = FpH * FTheta * k1;//1.25 //should be 0 to 1
+                double k2act = FpH * FTheta * k2;//1.25
 
-    double FS = Math.Pow(E, -b * (S_S04 / Water));	//1.31
-    double FThetaM = Math.Pow(E, ThetaAM + ThetaBM * temperatureInCelsius * (1 - 0.5 * (temperatureInCelsius / ThetaCM)));
-	double k3act=FThetaM*FpH*FS*k3; //1.32
-	double k4act=FTheta*FpH*(1-FS)*k4; //1.33
+                double hydrolysedCpool = k1act * CSlow + k2act * CFast;
+                double hydrolysedHpool = k1act * HSlow + k2act * HFast;
+                double hydrolysedOpool = k1act * OSlow + k2act * OFast;
 
-	//calculate C in CH4S. This will be instantaneously lost as CH4-C
-	CCH4S=0.375*k2act*SFast; //1.28
-	//calculate the H in CH4S
-	double HCH4S=0.333*CCH4S;//1.39
+                double FS = Math.Pow(E, -b * (S_S04 / Water));	//1.31
+                double FThetaM = Math.Pow(E, ThetaAM + ThetaBM * temperatureInCelsius * (1 - 0.5 * (temperatureInCelsius / ThetaCM)));
+                double k3act = FThetaM * FpH * FS * k3; //1.32
+                double k4act = FTheta * FpH * (1 - FS) * k4; //1.33
 
-	CGas=k3act*CVFA;//1.35	//anaerobic degradation of VFA by methanogens
+                //calculate C in CH4S. This will be instantaneously lost as CH4-C
+                CCH4S = 0.375 * k2act * SFast; //1.28
+                //calculate the H in CH4S
+                double HCH4S = 0.333 * CCH4S;//1.39
 
-	double Hgas=k3act * HVFA; //1.44
-	double Ogas=k3act * OVFA;//1.45
-	double CHCH4M=12*(CGas/24+Hgas/8-Ogas/64); //1.46
+                CGas = k3act * CVFA;//1.35	//anaerobic degradation of VFA by methanogens
 
-	CCO2_S=k4act*CVFA; //1.34  //CO2-C from oxidation by SO4
-//calculate the H utilised during oxidation of VFA by SO4. Oxygen is not budgetted here.
-	double HSO4=0.167*CCO2_S;//1.37
+                double Hgas = k3act * HVFA; //1.44
+                double Ogas = k3act * OVFA;//1.45
+                double CHCH4M = 12 * (CGas / 24 + Hgas / 8 - Ogas / 64); //1.46
 
-	CO2M=CGas-CHCH4M;//1.47	
-	CCH4=CCH4S+CHCH4M; //1.48	
+                CCO2_S = k4act * CVFA; //1.34  //CO2-C from oxidation by SO4
+                //calculate the H utilised during oxidation of VFA by SO4. Oxygen is not budgetted here.
+                double HSO4 = 0.167 * CCO2_S;//1.37
 
-    double KN = Math.Pow(10, -0.09018 - (2729.92 / temperatureInKelvin)); //1.53
-    double KH = Math.Pow(10, -1.69 + 1447.7 / temperatureInKelvin); //1.55
+                CO2M = CGas - CHCH4M;//1.47	
+                CCH4 = CCH4S + CHCH4M; //1.48	
 
-    if (Tan > 0.0)
-        ENH3 = 24 * 60 * 60 * surfaceArea * Tan / (Water * KH * (1 + Math.Pow(10, -slurrypH) / KN) * RA); //1.57
-    else
-        ENH3 = 0.0;
+                double KN = Math.Pow(10, -0.09018 - (2729.92 / temperatureInKelvin)); //1.53
+                double KH = Math.Pow(10, -1.69 + 1447.7 / temperatureInKelvin); //1.55
 
-//update C state variables
-	//! Update the carbon in the Inert pool
-	//GInert=0.0;
-	C_Inert+=GInert*hydrolysedCpool; //1.27
+                if (Tan > 0.0)
+                    ENH3 = 24 * 60 * 60 * surfaceArea * Tan / (Water * KH * (1 + Math.Pow(10, -slurrypH) / KN) * RA); //1.57
+                else
+                    ENH3 = 0.0;
 
-	//! Update the values of the carbon in the Fast and Slow pools is
-	CSlow*=(1-k1act); //1.24
-	CFast*=(1-k2act); //1.24 
-	double CAddVFA=(1-GInert)*hydrolysedCpool-CCH4S;//1.29
-	if (CAddVFA<0.0)
-	{
-        StringType message=new StringType("Negative VFA addition");
-       panic.Invoke(message);
-	}
-	CVFA=(1-(k3act+k4act))*CVFA+CAddVFA; //1.30
+                //update C state variables
+                //! Update the carbon in the Inert pool
+                //GInert=0.0;
+                C_Inert += GInert * hydrolysedCpool; //1.27
 
-	//update the H and O
-	HFast*=(1-k2act);//1.36
-	HSlow*=(1-k1act);//1.36
-	OFast*=(1-k2act);//1.36
-	OSlow*=(1-k1act);//1.36
-	double HAddInert=0.055*GInert*hydrolysedHpool;
-	HInert+=HAddInert; //1.37
-	double OAddInert=0.044*GInert*hydrolysedOpool;
-	OInert+=OAddInert;//1.38
-	HVFA+=hydrolysedHpool-(HAddInert+HCH4S+HSO4+Hgas); //1.42
-	OVFA+=hydrolysedOpool-(OAddInert+Ogas);//1.43
+                //! Update the values of the carbon in the Fast and Slow pools is
+                CSlow *= (1 - k1act); //1.24
+                CFast *= (1 - k2act); //1.24 
+                double CAddVFA = (1 - GInert) * hydrolysedCpool - CCH4S;//1.29
+                if (CAddVFA < 0.0)
+                {
+                    StringType message = new StringType("Negative VFA addition");
+                    panic.Invoke(message);
+                }
+                CVFA = (1 - (k3act + k4act)) * CVFA + CAddVFA; //1.30
 
-	//update S in Fast	
-	SFast*=(1-k2act);//1.59
-	S_S04-=2.667 * CCO2_S; //1.60
-	double SAddS2 = 2.667 * (CCO2_S + CCH4S);
-	S2_S+=SAddS2; //1.61
+                //update the H and O
+                HFast *= (1 - k2act);//1.36
+                HSlow *= (1 - k1act);//1.36
+                OFast *= (1 - k2act);//1.36
+                OSlow *= (1 - k1act);//1.36
+                double HAddInert = 0.055 * GInert * hydrolysedHpool;
+                HInert += HAddInert; //1.37
+                double OAddInert = 0.044 * GInert * hydrolysedOpool;
+                OInert += OAddInert;//1.38
+                HVFA += hydrolysedHpool - (HAddInert + HCH4S + HSO4 + Hgas); //1.42
+                OVFA += hydrolysedOpool - (OAddInert + Ogas);//1.43
 
-	//update the N
-	double NAddInert=0.1*GInert*hydrolysedCpool;
-	N_Inert+=NAddInert;//1.51
-	Tan+=k2act*NFast-(NAddInert+ENH3);// 1.52
-	NFast*=(1-k2act); //1.50
+                //update S in Fast	
+                SFast *= (1 - k2act);//1.59
+                S_S04 -= 2.667 * CCO2_S; //1.60
+                double SAddS2 = 2.667 * (CCO2_S + CCH4S);
+                S2_S += SAddS2; //1.61
 
-	if (Tan<0.0)
-	{
-		
-        StringType message = new StringType("Not enough TAN to enable the inert to be created or too much volatilisation");
-        panic.Invoke(message);
-	}
+                //update the N
+                double NAddInert = 0.1 * GInert * hydrolysedCpool;
+                N_Inert += NAddInert;//1.51
+                Tan += k2act * NFast - (NAddInert + ENH3);// 1.52
+                
+                if (NFast < 0)
+                    throw new System.ArgumentException("NFast is below 0");
+                NFast *= (1 - k2act); //1.50
+                if (NFast < 0)
+                    throw new System.ArgumentException("NFast is below 0");
+              
 
-    DoubleType value = new DoubleType();
-    value.Value = CCH4S;
-    CCH4SEvent.Invoke(value);
-    value.Value = CCO2_S;
-    CCO2_SEvent.Invoke(value);
-    value.Value = CGas;
-    CGasEvent.Invoke(value);
-    value.Value = ENH3;
-    ENH3Event.Invoke(value);
-    value.Value = CH4EM;
-    CH4EMEvent.Invoke(value);
-    value.Value = NN2O;
-    NN2OEvent.Invoke(value);
-    for (int i = 0; i < MyPaddock.Children.Count; i++)
-    {
-        Component item = MyPaddock.Children[i];
+               DoubleType value = new DoubleType();
+                value.Value = CCH4S;
+                if (CCH4S < 0)
+                    throw new System.ArgumentException("CCH4S is below 0");
+                CCH4SEvent.Invoke(value);
+                if (CCO2_S < 0)
+                    throw new System.ArgumentException("CCO2_S is below 0");
+                value.Value = CCO2_S;
+                CCO2_SEvent.Invoke(value);
+                value.Value = CGas;
+                if (CCH4S < 0)
+                    throw new System.ArgumentException("CCH4S is below 0");
+                CGasEvent.Invoke(value);
+                value.Value = ENH3;
+                if (ENH3 < 0)
+                    throw new System.ArgumentException("ENH3 is below 0");
+                ENH3Event.Invoke(value);
+                value.Value = CH4EM;
+                if (CH4EM < 0)
+                    throw new System.ArgumentException("CH4EM is below 0");
+                CH4EMEvent.Invoke(value);
+                value.Value = NN2O;
+                if (NN2O < 0)
+                    throw new System.ArgumentException("NN2O is below 0");
+                NN2OEvent.Invoke(value);
+                for (int i = 0; i < MyPaddock.Children.Count; i++)
+                {
+                    Component item = MyPaddock.Children[i];
 
-        if (item.Name.CompareTo("SurfaceOrganicMatter")==0)
-        {
-            Console.WriteLine("Sending manure to soil");
+                    if (item.Name.CompareTo("SurfaceOrganicMatter") == 0)
+                    {
+                  
 
-            AddFaecesType faeces = new AddFaecesType();
-            faeces.VolumePerDefaecation = GetMass();
-            faeces.AreaPerDefaecation = 0.0;
-            faeces.NO3N = 0;
-            faeces.AreaPerDefaecation = 0;
-            faeces.Defaecations = 0;
-            faeces.Eccentricity = 0;
-            faeces.NH4N = Tan;
-            faeces.NO3N = 0;
-            faeces.OMAshAlk = 0;
-            faeces.OMN = N_Inert + NFast;
-            faeces.OMP = 0;
-            faeces.OMS = SFast;
-            faeces.OMWeight = GetOM();
-            faeces.POXP = 0;
-            faeces.SO4S = S_S04;
-            item.Publish("add_faeces", faeces);
-        }
-    }
-            /*
-    AddFaecesType faeces1 = new AddFaecesType();
-    faeces1.NO3N = 0;
-    faeces1.AreaPerDefaecation = 0;
-    faeces1.Defaecations = 0;
-    faeces1.Eccentricity = 0;
-    faeces1.NH4N = 0;
-    faeces1.NO3N = 0;
-    faeces1.OMAshAlk = 0;
-    faeces1.OMN = 0;
-    faeces1.OMP = 0;
-    faeces1.OMS = 0;
-    faeces1.OMWeight = 0;
-    faeces1.POXP = 0;
-    faeces1.SO4S = 0;
-    faeces1.VolumePerDefaecation = 0;
-    add_faeces.Invoke(faeces1);*/
-    NN2O = 0;
- 
+                        AddFaecesType faeces = new AddFaecesType();
+                        faeces.VolumePerDefaecation = GetMass();
+                        if(faeces.VolumePerDefaecation<0)
+                         throw new System.ArgumentException("VolumePerDefaecation is below 0");
+                        faeces.AreaPerDefaecation = 0.0;
+                        faeces.NO3N = 0;
+                        faeces.AreaPerDefaecation = 0;
+                        faeces.Defaecations = 0;
+                        faeces.Eccentricity = 0;
+                        faeces.NH4N = Tan;
+                         if(faeces.NH4N<0)
+                         throw new System.ArgumentException("NH4N is below 0");
+                        faeces.NO3N = 0;
+                        faeces.OMAshAlk = 0;
+                        faeces.OMN = N_Inert + NFast;
+                             if(faeces.OMN<0)
+                         throw new System.ArgumentException("OMN is below 0");
+                        faeces.OMP = 0;
+                        faeces.OMS = SFast;
+                             if(faeces.OMS<0)
+                         throw new System.ArgumentException("OMS is below 0");
+                        faeces.OMWeight = GetOM();
+                             if(faeces.OMWeight<0)
+                         throw new System.ArgumentException("OMWeight is below 0");
+                        faeces.POXP = 0;
+                        faeces.SO4S = S_S04;
+                        item.Publish("add_faeces", faeces);
+                    }
+                }
+                /*
+        AddFaecesType faeces1 = new AddFaecesType();
+        faeces1.NO3N = 0;
+        faeces1.AreaPerDefaecation = 0;
+        faeces1.Defaecations = 0;
+        faeces1.Eccentricity = 0;
+        faeces1.NH4N = 0;
+        faeces1.NO3N = 0;
+        faeces1.OMAshAlk = 0;
+        faeces1.OMN = 0;
+        faeces1.OMP = 0;
+        faeces1.OMS = 0;
+        faeces1.OMWeight = 0;
+        faeces1.POXP = 0;
+        faeces1.SO4S = 0;
+        faeces1.VolumePerDefaecation = 0;
+        add_faeces.Invoke(faeces1);*/
+                NN2O = 0;
+
+            }
         }
 
     
