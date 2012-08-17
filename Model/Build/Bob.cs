4fa925d491795b06175f2a01ca4da3bca0ad851c
zur003 <eric.zurcher@csiro.au>
2012-08-17 05:08:28 +0000

    Change APSIM to accept a .apsim file. Also change the way Bob runs it's jobs - distributed builds and runs. bugid: 1578

--- Model/Build/Bob.cs
+++ Model/Build/Bob.cs
@@ -48,43 +48,69 @@ class Bob
                  string PatchFileName = DBGet("PatchFileName", Connection, JobID).ToString();
                  PatchFileName = PatchFileName.Replace('/', Path.DirectorySeparatorChar).Replace('\\', Path.DirectorySeparatorChar);
                   
-                 if (System.Environment.MachineName.ToUpper() != "BOB")
+                 if (PatchFileName.Contains("BobDean"))
                  {
-                    string NowString = DateTime.Now.ToString("yyyy-MM-dd hh:mm tt");
-                    DBUpdate("StartTime", NowString, Connection, JobID);
-                 }
+                    // Set some environment variables.
+                    System.Environment.SetEnvironmentVariable("JobID", JobID.ToString());
+                    System.Environment.SetEnvironmentVariable("PatchFileName", PatchFileName);
 
-                 // Update the builds database.
-                 DBUpdate("Status", "Running", Connection, JobID);
+                    // Open log file.
+                    string LogFileName = Path.ChangeExtension(PatchFileName, ".txt");
+                    StreamWriter Log = new StreamWriter(LogFileName);
+                    
+                    // Extract the patch.
+                    Run("Extracting patch: " + PatchFileName,
+                        "C:\\Program Files\\7-Zip\\7z.exe", 
+                        "x -y " + PatchFileName, 
+                        Log);
+
+                    // Run the patch.
+                    string CSCS = Assembly.GetCallingAssembly().Location;
+                    Run("Running patch...", CSCS, args[0], Log);
                  
-                 // Check the previous job to see if it has stalled. If so then set its 
-                 // status accordingly. Otherwise we get multiple "Running" status'.
-                 if (JobID > 0)
-                 {
-                    string PreviousStatus = DBGet("Status", Connection, JobID-1).ToString();
-                    if (PreviousStatus == "Running")
-                       DBUpdate("Status", "Aborted", Connection, JobID-1);
+                    // Write log file.
+                    Log.Close();
                  }
-                 
-                 // Set some environment variables.
-                 System.Environment.SetEnvironmentVariable("JobID", JobID.ToString());
-                 System.Environment.SetEnvironmentVariable("PatchFileName", Path.GetFileNameWithoutExtension(PatchFileName));
-                 System.Environment.SetEnvironmentVariable("PatchFileNameFull", PatchFileName);
-                 
-                 // Extract the patch.
-                 Console.WriteLine("Extracting patch: " + PatchFileName);
-                 string UnZip = "C:\\Program Files\\7-Zip\\7z.exe";
-                 Process P = Process.Start(UnZip, "x -y " + PatchFileName);
-                 P.WaitForExit();
-                 
-                 // *********************************************
-                 // Now we can run the nested script on the patch
-                 // *********************************************
-                 Console.WriteLine("Running patch: " + PatchFileName);
-                 string CSCS = Assembly.GetCallingAssembly().Location;
-                 P = Process.Start(CSCS, args[0]);
-                 P.WaitForExit();
-                 
+                 else 
+                 {
+                    if (System.Environment.MachineName.ToUpper() != "BOB")
+                    {
+                       string NowString = DateTime.Now.ToString("yyyy-MM-dd hh:mm tt");
+                       DBUpdate("StartTime", NowString, Connection, JobID);
+                    }
+
+                    // Update the builds database.
+                    DBUpdate("Status", "Running", Connection, JobID);
+                    
+                    // Check the previous job to see if it has stalled. If so then set its 
+                    // status accordingly. Otherwise we get multiple "Running" status'.
+                    if (JobID > 0)
+                    {
+                       string PreviousStatus = DBGet("Status", Connection, JobID-1).ToString();
+                       if (PreviousStatus == "Running")
+                          DBUpdate("Status", "Aborted", Connection, JobID-1);
+                    }
+                    
+                    // Set some environment variables.
+                    System.Environment.SetEnvironmentVariable("JobID", JobID.ToString());
+                    System.Environment.SetEnvironmentVariable("PatchFileName", Path.GetFileNameWithoutExtension(PatchFileName));
+                    System.Environment.SetEnvironmentVariable("PatchFileNameFull", PatchFileName);
+                    
+                    // Extract the patch.
+                    Console.WriteLine("Extracting patch: " + PatchFileName);
+                    Process P = Process.Start("C:\\Program Files\\7-Zip\\7z.exe", "x -y " + PatchFileName);
+                    P.WaitForExit();
+                    
+                    // *********************************************
+                    // Now we can run the nested script on the patch
+                    // *********************************************
+                    Console.WriteLine("Running patch: " + PatchFileName);
+                    string CSCS = Assembly.GetCallingAssembly().Location;
+                    P = Process.Start(CSCS, args[0]);
+                    P.WaitForExit();
+
+               
+                 }
                  Console.WriteLine("Waiting for a patch...");
              }
              else
@@ -196,6 +222,65 @@ class Bob
          throw new Exception("Cannot execute reader query to SQL server: www.apsim.info");
       }    
    
+      // Returns StdOut.
+      static string Run(string Name, string Executable, string Arguments, StreamWriter Log)
+      {
+         Executable = ReplaceEnvironmentVariables(Executable);
+         if (!File.Exists(Executable))
+         {
+            if (Path.DirectorySeparatorChar == '/') 
+               Executable = Path.ChangeExtension(Executable, "");   // linux - remove extension
+            Executable = FindFileOnPath(Executable);
+         }
+         Arguments = ReplaceEnvironmentVariables(Arguments);
+         Process P = RunProcess(Executable, Arguments, Directory.GetCurrentDirectory());
+         string StdOut = CheckProcessExitedProperly(P);      
+         if (P.ExitCode != 0)
+         {
+            Log.WriteLine(Name + " [Fail]");
+            Log.WriteLine(IndentText(StdOut, 4));
+         }
+         else
+         {
+            Log.WriteLine(Name + " [Pass]");
+         }
+         return StdOut;
+      }
+      
+      static Process RunProcess(string Executable, string Arguments, string JobFolder)
+      {
+         if (!File.Exists(Executable))
+             throw new System.Exception("Cannot execute file: " + Executable + ". File not found.");
+         Process PlugInProcess = new Process();
+         PlugInProcess.StartInfo.FileName = Executable;
+         PlugInProcess.StartInfo.Arguments = Arguments;
+         PlugInProcess.StartInfo.UseShellExecute = false;
+         PlugInProcess.StartInfo.CreateNoWindow = true;
+         if (!PlugInProcess.StartInfo.UseShellExecute)
+         {
+             PlugInProcess.StartInfo.RedirectStandardOutput = true;
+             PlugInProcess.StartInfo.RedirectStandardError = true;
+         }
+         PlugInProcess.StartInfo.WorkingDirectory = JobFolder;
+         PlugInProcess.Start();
+         return PlugInProcess;
+      }
+      
+      static string CheckProcessExitedProperly(Process PlugInProcess)
+      {
+         if (!PlugInProcess.StartInfo.UseShellExecute)
+         {
+             string msg = PlugInProcess.StandardOutput.ReadToEnd();
+             PlugInProcess.WaitForExit();
+             if (PlugInProcess.ExitCode != 0)
+                 msg += PlugInProcess.StandardError.ReadToEnd();
+             return msg;
+         }
+         else
+             return "";
+      }   
+       
+   
       /// <summary>
       /// Find the specified file on the path.
       /// </summary>
@@ -223,4 +308,53 @@ class Bob
          throw new Exception("Cannot find: " + FileName + " on the system path.");
       }   
    
+      // -------------------------------------------------------
+      // Indent the specified string a certain number of spaces.
+      // -------------------------------------------------------
+      static string IndentText(string St, int numChars)
+      {
+         string space = new string(' ', numChars);
+         return space + St.Replace("\r\n", "\r\n" + space);
+      }   
+   
+   
+      /// <summary>
+      /// Look through the specified string for an environment variable name surrounded by
+      /// % characters. Replace them with the environment variable value.
+      /// </summary>
+      static string ReplaceEnvironmentVariables(string CommandLine)
+      {
+         if (CommandLine == null)
+             return CommandLine;
+
+         int PosPercent = CommandLine.IndexOf('%');
+         while (PosPercent != -1)
+         {
+             string Value = null;
+             int EndVariablePercent = CommandLine.IndexOf('%', PosPercent + 1);
+             if (EndVariablePercent != -1)
+             {
+                 string VariableName = CommandLine.Substring(PosPercent + 1, EndVariablePercent - PosPercent - 1);
+                 Value = System.Environment.GetEnvironmentVariable(VariableName);
+                 if (Value == null)
+                     Value = System.Environment.GetEnvironmentVariable(VariableName, EnvironmentVariableTarget.User);
+             }
+
+             if (Value != null)
+             {
+                 CommandLine = CommandLine.Remove(PosPercent, EndVariablePercent - PosPercent + 1);
+                 CommandLine = CommandLine.Insert(PosPercent, Value);
+                 PosPercent = PosPercent + 1;
+             }
+
+             else
+                 PosPercent = PosPercent + 1;
+
+             if (PosPercent >= CommandLine.Length)
+                 PosPercent = -1;
+             else
+                 PosPercent = CommandLine.IndexOf('%', PosPercent);
+         }
+         return CommandLine;
+      }    
    }
