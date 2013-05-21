4c76bd8e095a53cf48e22ef8669fb0cebc60c2aa
devoilp <p.devoil@uq.edu.au>
2013-05-21 07:57:05 +0000

    fix the self extracting exes made by bob bugid: 1720

--- Release/Release.bat
+++ Release/Release.bat
@@ -10,8 +10,10 @@ copy  %APSIM%\Model\*.exe %dest%\Model
 copy  %APSIM%\Model\*.x   %dest%\Model
 mkdir %dest%\Model\TclLink
 copy  %APSIM%\Model\TclLink\CIDataTypes.tcl %dest%\Model\TclLink
-xcopy  /e %APSIM%\Model\TclLink\bin %dest%\Model\TclLink
-xcopy  /e %APSIM%\Model\TclLink\lib %dest%\Model\TclLink
+mkdir %dest%\Model\TclLink\bin
+xcopy  /e %APSIM%\Model\TclLink\bin %dest%\Model\TclLink\bin
+mkdir %dest%\Model\TclLink\lib
+xcopy  /e %APSIM%\Model\TclLink\lib %dest%\Model\TclLink\lib
 
 mkdir %dest%\UserInterface
 copy  %APSIM%\UserInterface\*.xml %dest%\UserInterface
