81486d02cd3801b4ac5634eb7e507f83daa08902
Robert.Zyskowski <robert.zyskowski@plantandfood.co.nz>
2013-09-16 00:17:07 +0000

    MultiCrop Single purpose + functions bugid: 1015

---
+++ Model/Plant2/Functions/SigmoidFunction2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using CSGeneral;
+
+[Description("Takes the value of the child as the x value and returns the y value from a sigmoid of the form y = Xmax * 1/1+exp(-(x-Xo)/b)")]
+public class SigmoidFunction2 : Function
+{
+    [Link]
+    public Function Ymax = null;
+    [Link]
+    public Function XValue = null;
+  
+    [Param]
+    private double Xo = 1.0;
+    [Param]
+    private double b = 1.0;
+    
+    
+    [Output]
+    public override double Value
+    {
+        get
+        {
+
+            try
+            {
+                return Ymax.Value * 1 / (1 + Math.Exp(-(XValue.Value - Xo) / b));
+            }
+            catch (Exception E)
+            {
+                throw new Exception("Error with values to Sigmoid function");
+            }
+         }
+    }
+
+}
+
