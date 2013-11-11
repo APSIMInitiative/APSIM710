8f8bd652642f28d0e22c0e88cf591669aef34027
devoilp <p.devoil@uq.edu.au>
2013-11-11 05:35:06 +0000

    fix xcopy pausing in buildall.xml bugid: 1377

--- Release/Release.bat
+++ Release/Release.bat
@@ -21,12 +21,12 @@ mkdir %dest%\UserInterface\ToolBoxes
 copy  %APSIM%\UserInterface\ToolBoxes\*.xml %dest%\UserInterface\ToolBoxes
 
 if [%1] == [] goto noargs
-  "C:\Program Files\7-Zip\7z.exe" a -mx=7 -mmt=on -sfx %1.binaries.Win32.exe %dest%
+  "C:\Program Files\7-Zip\7z.exe" a -mx=7 -mmt=on -sfx %1.binaries.WINDOWS.exe %dest%
    goto done
 
 :noargs
   %APSIM%\Model\Build\VersionInfo.bat
-  "C:\Program Files\7-Zip\7z.exe" a -mx=7 -mmt=on -sfx Apsim%MAJOR_VERSION%%MINOR_VERSION%-r%BUILD_NUMBER%.binaries.Win32.exe %dest%
+  "C:\Program Files\7-Zip\7z.exe" a -mx=7 -mmt=on -sfx Apsim%MAJOR_VERSION%.%MINOR_VERSION%-r%BUILD_NUMBER%.binaries.WINDOWS.exe %dest%
 
 :done
 del /s /q /f Temp
