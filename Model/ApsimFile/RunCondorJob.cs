885af6c8e4816d02169fa603b726558ad1256b5b
devoilp <p.devoil@uq.edu.au>
2014-03-07 07:39:39 +0000

    process factorials correctly in run on cluster bugid: 1771

--- Model/ApsimFile/RunCondorJob.cs
+++ Model/ApsimFile/RunCondorJob.cs
@@ -83,10 +83,10 @@ namespace ApsimFile
                 ApsimCondor a = new ApsimCondor();
                 a.Credentials = new System.Net.NetworkCredential(username, password);
                 string s = a.AddJob(new string [] {"/home/" + username + "/" + Path.GetFileName(localzip)});
+				// Now what? Should monitor "s" for completion
             }
             return;
         }
-
         // Add individual .apsim files to the job
         private void AddFiles(XmlNode job, List<string> FilesToRun, ProgressNotifier Notifier)
         {
@@ -94,59 +94,47 @@ namespace ApsimFile
             foreach (string FileName in FilesToRun)
             {
                 Notifier(0, "Reading " + FileName);
-                XmlNode apsimFileNode = job.AppendChild(job.OwnerDocument.CreateElement("apsimfile"));
-                XmlHelper.SetAttribute(apsimFileNode, "source", FileName);
                 XmlDocument Doc = new XmlDocument();
                 Doc.Load(FileName);
-                AddSimulation(apsimFileNode, Doc, Notifier);
-                Doc.Save(Path.Combine(WorkingFolder, Path.GetFileName(FileName)));
-            }
-        }
+                ApsimFile F = null;
+                if (XmlHelper.FindByType(Doc.DocumentElement, "factorial") != null) {
+				   F = new ApsimFile();
+				   F.Open(Doc.DocumentElement);
+				}
 
-        // 
-        /// <summary>
-        ///  Bust open a .apsim file and process all simulations inside. Some simulations
-        ///  have simulations inside simulations (ie. factorials) 
-        /// </summary>
-        /// <param name="fileInJob"></param>
-        /// <param name="FileName"></param>
-        /// <param name="Notifier"></param>
-        public void AddSimulation(XmlNode fileInJob, XmlDocument simulationDoc, ProgressNotifier Notifier)
-        {
-            List<XmlNode> simulations = new List<XmlNode>();
-            XmlHelper.FindAllRecursivelyByType(simulationDoc.DocumentElement, "simulation", ref simulations);
-            foreach (XmlNode simulation in simulations)
-            {
-                if (XmlHelper.Attribute(simulation, "enabled") != "no")
-                {
-                    //Component Simulation = F.Find(SimulationPath);
-                    //if (F.FactorComponent != null && F.FactorComponent.ChildNodes.Count > 0)   // Fixme - factorcompoennt should be a property if the simulation, not the file???
-                    //{
-                    //List<SimFactorItem> SimItems = new List<SimFactorItem>();
-                    //Factor.ProcessSimulationFactorials(SimItems, F, F.FactorComponent, SimulationPath);
-                    //foreach (SimFactorItem simItem in SimItems)
-                    //    simFiles.Add(simItem.SimFileName);
-                    //}
-                    XmlNode simulationNode = fileInJob.AppendChild(fileInJob.OwnerDocument.CreateElement("simulation"));
-                    XmlHelper.SetAttribute(simulationNode, "name", XmlHelper.Name(simulation));
-                    foreach (XmlNode node in simulation.SelectNodes(".//filename"))
-                        if (XmlHelper.Attribute(node, "output") != "yes")
-                        {
-                            string src = Configuration.RemoveMacros(node.InnerText);
-                            string dest = Path.GetFileName(src);
-                            XmlNode input = simulationNode.AppendChild(simulationNode.OwnerDocument.CreateElement("input"));
-                            XmlHelper.SetAttribute(input, "source", src);
-                            XmlHelper.SetAttribute(input, "name", dest);
-                            node.InnerText = dest;
-                            if (!File.Exists(Path.Combine(WorkingFolder, dest)))
-                                File.Copy(src, Path.Combine(WorkingFolder, dest));
-                        }
-                        else
-                        {
-                            XmlNode output = simulationNode.AppendChild(simulationNode.OwnerDocument.CreateElement("output"));
-                            XmlHelper.SetAttribute(output, "name", node.InnerText);
-                        }
-                }
+				List<XmlNode> simulations = new List<XmlNode>();
+                XmlHelper.FindAllRecursivelyByType(Doc.DocumentElement, "simulation", ref simulations);
+
+				foreach (XmlNode simulation in simulations)
+					if (XmlHelper.Attribute (simulation, "enabled") != "no") {
+						List<string> SimsToRun = new List<string> ();
+						if (F != null)
+							ApsimFile.ExpandSimsToRun (F.Find (XmlHelper.Name (simulation)), ref SimsToRun);
+						else
+							SimsToRun.Add (XmlHelper.Name (simulation));
+					
+						foreach (string simToRun in SimsToRun) {
+							XmlNode apsimFileNode = job.AppendChild (job.OwnerDocument.CreateElement ("apsimfile"));
+							XmlHelper.SetAttribute (apsimFileNode, "source", FileName);
+							XmlNode simulationNode = apsimFileNode.AppendChild (apsimFileNode.OwnerDocument.CreateElement ("simulation"));
+							XmlHelper.SetAttribute (simulationNode, "name", simToRun);
+							XmlHelper.SetAttribute (simulationNode, "source", XmlHelper.Attribute (apsimFileNode, "source"));
+							foreach (XmlNode node in simulation.SelectNodes(".//filename"))
+								if (XmlHelper.Attribute (node, "output") != "yes") {
+									string src = Configuration.RemoveMacros (node.InnerText);
+									string dest = Path.GetFileName (src);
+									XmlNode input = simulationNode.AppendChild (simulationNode.OwnerDocument.CreateElement ("input"));
+									XmlHelper.SetAttribute (input, "source", src); // <<<<<<<<<<<< WRONG. could be a factorial "target". FIXME
+									XmlHelper.SetAttribute (input, "name", dest);
+									node.InnerText = dest;
+									if (!File.Exists (Path.Combine (WorkingFolder, dest)))
+										File.Copy (src, Path.Combine (WorkingFolder, dest));
+								} else {
+									XmlNode output = simulationNode.AppendChild (simulationNode.OwnerDocument.CreateElement ("output"));
+									XmlHelper.SetAttribute (output, "name", node.InnerText);
+								}
+						}
+					}
             }
         }
 
@@ -179,6 +167,7 @@ namespace ApsimFile
             ExeWriter.NewLine = "\n";
             ExeWriter.WriteLine("#!/bin/bash");
             ExeWriter.WriteLine("for i in $@; do chmod +x $i; ./$i; rm -f $i; done");
+			ExeWriter.WriteLine("if [ -d Temp ]; then rm -rf Temp; fi");
             ExeWriter.Close();
             File.Copy(Path.Combine(WorkingFolder, "Apsim.LINUX.INTEL.bat"), Path.Combine(WorkingFolder, "Apsim.LINUX.X86_64.bat"));
 
@@ -190,6 +179,8 @@ namespace ApsimFile
             ExeWriter.WriteLine("SHIFT");
             ExeWriter.WriteLine("GOTO TOP");
             ExeWriter.WriteLine(":END");
+            ExeWriter.WriteLine("rmdir /s /q Temp");
+			
             ExeWriter.Close();
             File.Copy(Path.Combine(WorkingFolder, "Apsim.WINDOWS.INTEL.bat"), Path.Combine(WorkingFolder, "Apsim.WINDOWS.X86_64.bat"));
 
@@ -215,22 +206,17 @@ namespace ApsimFile
                     LinuxExeWriter.WriteLine("#!/bin/bash");
                 }
 
-                string apsimFile = Path.GetFileName(XmlHelper.Attribute(simNode.ParentNode, "source"));
-                if (!inputfiles.Contains(apsimFile))
-                {
-                    inputfiles.Add(apsimFile);
-                }
-
+                string apsimFile = Path.GetFileName(XmlHelper.Attribute(simNode, "source"));
                 WinExeWriter.WriteLine(".\\Temp\\Model\\Apsim.exe \"" + apsimFile + "\" \"Simulation=" + XmlHelper.Attribute(simNode, "name") + "\"");
                 LinuxExeWriter.WriteLine("./Temp/Model/Apsim.exe \"" + apsimFile + "\" \"Simulation=" + XmlHelper.Attribute(simNode, "name") + "\"");
+                if (!inputfiles.Contains(apsimFile))
+                    inputfiles.Add(apsimFile);
 
-                foreach (XmlNode inputNode in simNode.SelectNodes(".//input"))
-                {
-                    string localName = XmlHelper.Attribute(inputNode, "name");
+				foreach (XmlNode inputNode in simNode.SelectNodes(".//input"))
                     if (!inputfiles.Contains(XmlHelper.Attribute(inputNode, "name")))
                         inputfiles.Add(XmlHelper.Attribute(inputNode, "name"));
-                }
-                numSims++;
+
+				numSims++;
                 if (numSims >= numberSimsPerJob)
                 {
                     SubWriter.WriteLine("transfer_input_files = " + string.Join(",", inputfiles));
@@ -287,66 +273,6 @@ namespace ApsimFile
             return Path.Combine(DestinationFolder, ZipFileName);
         }
 #if false
-		public static void CreateJobFile (ApsimFile F, string JobFileName, string EmailAddress, string ApsimVersion)
-		{
-			string UserName = EmailAddress;
-			if (!UserName.Contains ("@")) {
-				throw new Exception ("Invalid email address: " + EmailAddress);
-			}
-			UserName = UserName.Remove (UserName.IndexOf ("@"));
-			
-			XmlDocument Doc = new XmlDocument ();
-			XmlNode JobNode = Doc.AppendChild (Doc.CreateElement ("Job"));
-			XmlHelper.SetName (JobNode, UserName);
-			
-			// Create <localcommand> node
-			XmlHelper.SetValue (JobNode, "LocalCommand/CommandLine", "\"c:\\Program files\\Apsim" + ApsimVersion.Replace (".", "") + "\\Model\\ApsimToSim.exe\" \"%jobfolder%\\" + Path.GetFileName (F.FileName) + "\"");
-			
-			// Create <cluster> node
-			XmlHelper.SetValue (JobNode, "Cluster/CommandLine", "Job.bat %f");
-			XmlHelper.SetValue (JobNode, "Cluster/NiceUser", "Yes");
-			XmlNode ClusterNode = XmlHelper.Find (JobNode, "Cluster");
-			XmlHelper.SetAttribute (ClusterNode, "foreach", "*.sim");
-			
-			// zip up the outputs.
-			string ZipFileName = CalcZipFileName ();
-			XmlHelper.SetValue (JobNode, "Zip/To", ZipFileName);
-			
-			// ftp the zip outputs to apsrunet2.
-			XmlHelper.SetValue (JobNode, "Ftp/From", ZipFileName);
-			XmlHelper.SetValue (JobNode, "Ftp/ToSite", "apsrunet2.apsim.info");
-			XmlHelper.SetValue (JobNode, "Ftp/ToFolder", "Cluster");
-			
-			// Create an email node.
-			XmlHelper.SetValue (JobNode, "Email/Subject", "Your cluster job has been completed");
-			XmlHelper.SetValue (JobNode, "Email/To", EmailAddress);
-			string Url = "http://www.apsim.info/Cluster/" + ZipFileName;
-			XmlHelper.SetValue (JobNode, "Email/Body", "To access your job outputs, goto <a href=\"" + Url + "\">" + Url + "</a>");
-			
-			// Create a log node.
-			XmlHelper.SetValue (JobNode, "Log/Description", "Cluster run by " + EmailAddress + ", Cluster");
-			
-			Doc.Save (JobFileName);
-		}
-
-
-		private static void SendToWebService (string ZipFileName, string Password)
-		{
-			FileInfo fInfo = new FileInfo (ZipFileName);
-			long numBytes = fInfo.Length;
-			
-			FileStream fStream = new FileStream (ZipFileName, FileMode.Open, FileAccess.Read);
-			BinaryReader br = new BinaryReader (fStream);
-			byte[] Data = br.ReadBytes ((int)numBytes);
-			br.Close ();
-			
-//		UIBits.ClusterService.Cluster s = new UIBits.ClusterService.Cluster ();
-//		s.Timeout = 300000;
-//		string msg = s.UploadFile (Data, Path.GetFileName (ZipFileName), Password);
-//		if (msg != "OK") {
-//			throw new Exception ("Error: " + msg);
-//		}
-		}
 
 ////##########		TEST
 using System;
