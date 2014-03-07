6af4256b2da43633dc26b2a5cbd5ff2896cb80d4
hol353 <dean.holzworth@csiro.au>
2014-03-07 02:37:11 +0000

    Allow easy entering of multiple values into factorial for non manager components bugid: 1836

--- Model/CSUserInterface/Factor.Designer.cs
+++ Model/CSUserInterface/Factor.Designer.cs
@@ -28,128 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-         this.pnlVariable = new System.Windows.Forms.Panel();
-         this.label2 = new System.Windows.Forms.Label();
-         this.gridManager = new UIBits.EnhancedGrid();
-         this.label1 = new System.Windows.Forms.Label();
-         this.colActive = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-         this.colManager = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.colParameters = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-         this.FactorTargets = new CSUserInterface.FactorTargets();
-         this.pnlVariable.SuspendLayout();
-         ((System.ComponentModel.ISupportInitialize)(this.gridManager)).BeginInit();
-         this.SuspendLayout();
-         // 
-         // MyHelpLabel
-         // 
-         this.MyHelpLabel.Size = new System.Drawing.Size(599, 16);
-         // 
-         // pnlVariable
-         // 
-         this.pnlVariable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.components = new System.ComponentModel.Container();
+            this.pnlVariable = new System.Windows.Forms.Panel();
+            this.csvLabel = new System.Windows.Forms.Label();
+            this.gridManager = new UIBits.EnhancedGrid();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colActive = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colManager = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colParameters = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.FactorTargets = new CSUserInterface.FactorTargets();
+            this.pnlVariable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridManager)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MyHelpLabel
+            // 
+            this.MyHelpLabel.Size = new System.Drawing.Size(599, 16);
+            // 
+            // pnlVariable
+            // 
+            this.pnlVariable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-         this.pnlVariable.Controls.Add(this.label2);
-         this.pnlVariable.Controls.Add(this.gridManager);
-         this.pnlVariable.Controls.Add(this.label1);
-         this.pnlVariable.Location = new System.Drawing.Point(-1, 157);
-         this.pnlVariable.Name = "pnlVariable";
-         this.pnlVariable.Size = new System.Drawing.Size(588, 384);
-         this.pnlVariable.TabIndex = 5;
-         // 
-         // label2
-         // 
-         this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-         this.label2.AutoSize = true;
-         this.label2.Location = new System.Drawing.Point(389, 344);
-         this.label2.Name = "label2";
-         this.label2.Size = new System.Drawing.Size(196, 13);
-         this.label2.TabIndex = 3;
-         this.label2.Text = "Parameters should be comma separated";
-         // 
-         // gridManager
-         // 
-         this.gridManager.AllowUserToAddRows = false;
-         this.gridManager.AllowUserToDeleteRows = false;
-         this.gridManager.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pnlVariable.Controls.Add(this.csvLabel);
+            this.pnlVariable.Controls.Add(this.gridManager);
+            this.pnlVariable.Controls.Add(this.label1);
+            this.pnlVariable.Location = new System.Drawing.Point(-1, 157);
+            this.pnlVariable.Name = "pnlVariable";
+            this.pnlVariable.Size = new System.Drawing.Size(588, 384);
+            this.pnlVariable.TabIndex = 5;
+            // 
+            // csvLabel
+            // 
+            this.csvLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.csvLabel.AutoSize = true;
+            this.csvLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csvLabel.Location = new System.Drawing.Point(232, 0);
+            this.csvLabel.Name = "csvLabel";
+            this.csvLabel.Size = new System.Drawing.Size(353, 13);
+            this.csvLabel.TabIndex = 3;
+            this.csvLabel.Text = "Multiple parameter values can be entered (comma separated)";
+            this.csvLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // gridManager
+            // 
+            this.gridManager.AllowUserToAddRows = false;
+            this.gridManager.AllowUserToDeleteRows = false;
+            this.gridManager.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-         this.gridManager.BackgroundColor = System.Drawing.SystemColors.Window;
-         this.gridManager.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-         this.gridManager.DataSourceTable = null;
-         this.gridManager.Location = new System.Drawing.Point(2, 18);
-         this.gridManager.Name = "gridManager";
-         this.gridManager.RowHeadersVisible = false;
-         this.gridManager.Size = new System.Drawing.Size(586, 314);
-         this.gridManager.TabIndex = 2;
-         // 
-         // label1
-         // 
-         this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(1, 0);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(95, 13);
-         this.label1.TabIndex = 1;
-         this.label1.Text = "Manager Variables";
-         // 
-         // colActive
-         // 
-         this.colActive.HeaderText = "";
-         this.colActive.Name = "colActive";
-         this.colActive.Width = 20;
-         // 
-         // colManager
-         // 
-         this.colManager.HeaderText = "Manager Variables";
-         this.colManager.Name = "colManager";
-         this.colManager.ReadOnly = true;
-         this.colManager.Width = 120;
-         // 
-         // colParameters
-         // 
-         this.colParameters.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-         this.colParameters.HeaderText = "Parameters";
-         this.colParameters.MinimumWidth = 20;
-         this.colParameters.Name = "colParameters";
-         // 
-         // Column1
-         // 
-         this.Column1.HeaderText = "Column1";
-         this.Column1.Name = "Column1";
-         // 
-         // Column2
-         // 
-         this.Column2.HeaderText = "Column2";
-         this.Column2.Name = "Column2";
-         // 
-         // FactorTargets
-         // 
-         this.FactorTargets.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.gridManager.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.gridManager.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridManager.DataSourceTable = null;
+            this.gridManager.Location = new System.Drawing.Point(2, 18);
+            this.gridManager.Name = "gridManager";
+            this.gridManager.RowHeadersVisible = false;
+            this.gridManager.Size = new System.Drawing.Size(586, 314);
+            this.gridManager.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Variables";
+            // 
+            // colActive
+            // 
+            this.colActive.HeaderText = "";
+            this.colActive.Name = "colActive";
+            this.colActive.Width = 20;
+            // 
+            // colManager
+            // 
+            this.colManager.HeaderText = "Manager Variables";
+            this.colManager.Name = "colManager";
+            this.colManager.ReadOnly = true;
+            this.colManager.Width = 120;
+            // 
+            // colParameters
+            // 
+            this.colParameters.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colParameters.HeaderText = "Parameters";
+            this.colParameters.MinimumWidth = 20;
+            this.colParameters.Name = "colParameters";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // FactorTargets
+            // 
+            this.FactorTargets.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-         this.FactorTargets.AutoScroll = true;
-         this.FactorTargets.BackColor = System.Drawing.SystemColors.Window;
-         this.FactorTargets.HelpText = "";
-         this.FactorTargets.Location = new System.Drawing.Point(0, 2);
-         this.FactorTargets.Margin = new System.Windows.Forms.Padding(0);
-         this.FactorTargets.Name = "FactorTargets";
-         this.FactorTargets.Size = new System.Drawing.Size(587, 150);
-         this.FactorTargets.TabIndex = 2;
-         // 
-         // Factor
-         // 
-         this.Controls.Add(this.FactorTargets);
-         this.Controls.Add(this.pnlVariable);
-         this.Name = "Factor";
-         this.Size = new System.Drawing.Size(599, 541);
-         this.Controls.SetChildIndex(this.MyHelpLabel, 0);
-         this.Controls.SetChildIndex(this.pnlVariable, 0);
-         this.Controls.SetChildIndex(this.FactorTargets, 0);
-         this.pnlVariable.ResumeLayout(false);
-         this.pnlVariable.PerformLayout();
-         ((System.ComponentModel.ISupportInitialize)(this.gridManager)).EndInit();
-         this.ResumeLayout(false);
+            this.FactorTargets.AutoScroll = true;
+            this.FactorTargets.BackColor = System.Drawing.SystemColors.Window;
+            this.FactorTargets.HelpText = "";
+            this.FactorTargets.Location = new System.Drawing.Point(0, 2);
+            this.FactorTargets.Margin = new System.Windows.Forms.Padding(0);
+            this.FactorTargets.Name = "FactorTargets";
+            this.FactorTargets.Size = new System.Drawing.Size(587, 150);
+            this.FactorTargets.TabIndex = 2;
+            // 
+            // Factor
+            // 
+            this.Controls.Add(this.FactorTargets);
+            this.Controls.Add(this.pnlVariable);
+            this.Name = "Factor";
+            this.Size = new System.Drawing.Size(599, 541);
+            this.Controls.SetChildIndex(this.MyHelpLabel, 0);
+            this.Controls.SetChildIndex(this.pnlVariable, 0);
+            this.Controls.SetChildIndex(this.FactorTargets, 0);
+            this.pnlVariable.ResumeLayout(false);
+            this.pnlVariable.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridManager)).EndInit();
+            this.ResumeLayout(false);
 
         }
 
@@ -164,7 +167,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colParameters;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Column2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label csvLabel;
 
 
     }
--- Model/CSUserInterface/Factor.cs
+++ Model/CSUserInterface/Factor.cs
@@ -14,7 +14,7 @@ namespace CSUserInterface
     public partial class Factor : Controllers.BaseView
     {
         DataTable Table = null;
-        
+
         public Factor()
         {
             InitializeComponent();
@@ -48,41 +48,62 @@ namespace CSUserInterface
                 if (variablesNode == null)
                     variablesNode = Data.AppendChild(Data.OwnerDocument.CreateElement("vars"));
                 variablesNode.RemoveAll();
-                foreach (DataRow row in Table.Rows)
+                if (Table.Rows.Count >= 10000)
                 {
-                    if ((bool)row[0])
+                    string[] values = DataTableUtility.GetColumnAsStrings(Table, Table.Columns[0].ColumnName);
+                    string csvValue = "";
+                    foreach (string value in values)
                     {
-                        XmlNode varNode = variablesNode.AppendChild(Data.OwnerDocument.CreateElement(row[1].ToString()));
-                        varNode.InnerText = row[3].ToString();
+                        if (value != "")
+                        {
+                            if (csvValue != "")
+                                csvValue += ",";
+                            csvValue += value;
+                        }
+                        else
+                            break;
+                    }
+                    XmlNode varNode = variablesNode.AppendChild(Data.OwnerDocument.CreateElement(Table.Columns[0].ColumnName));
+                    varNode.InnerText = csvValue;
+                }
+                else
+                {
+                    foreach (DataRow row in Table.Rows)
+                    {
+                        if ((bool)row[0])
+                        {
+                            XmlNode varNode = variablesNode.AppendChild(Data.OwnerDocument.CreateElement(row[1].ToString()));
+                            varNode.InnerText = row[3].ToString();
+                        }
                     }
                 }
             }
         }
-        
+
         public override bool OnDropData(StringCollection SourcePaths, string FullXML)
         {
-           //if there are no children, then add the droppednodes as targets
-           //don't add to targets if there is existing children
-           //don't add to targets if the source is from the Factorial Tree
-           ApsimFile.Component thisComp = Controller.ApsimData.Find(NodePath);
-           //if (thisComp.ChildNodes.Count == 0)
-           {
-              FactorTargets.AddTargets(SourcePaths);
-           }
-           thisComp.Add(FullXML);
-           OnRefresh();
-           return true;
-        }
-         public void LoadVariableTypes(XmlNode metNode, List<string> variableTypes)
-         {
-            if(metNode != null && variableTypes != null)
+            //if there are no children, then add the droppednodes as targets
+            //don't add to targets if there is existing children
+            //don't add to targets if the source is from the Factorial Tree
+            ApsimFile.Component thisComp = Controller.ApsimData.Find(NodePath);
+            //if (thisComp.ChildNodes.Count == 0)
             {
-               foreach (XmlNode varNode in XmlHelper.ChildNodes(metNode, "facvar"))
-               {
-                  variableTypes.Add(varNode.InnerText.ToLower());
-               }
+                FactorTargets.AddTargets(SourcePaths);
             }
-         }
+            thisComp.Add(FullXML);
+            OnRefresh();
+            return true;
+        }
+        public void LoadVariableTypes(XmlNode metNode, List<string> variableTypes)
+        {
+            if (metNode != null && variableTypes != null)
+            {
+                foreach (XmlNode varNode in XmlHelper.ChildNodes(metNode, "facvar"))
+                {
+                    variableTypes.Add(varNode.InnerText.ToLower());
+                }
+            }
+        }
 
         public void LoadManagerVariables()
         {
@@ -92,17 +113,19 @@ namespace CSUserInterface
             ApsimFile.Component thisComp = Controller.ApsimData.Find(NodePath);
             if (thisComp.ChildNodes.Count == 1)
             {
-               ApsimFile.Component childComp = thisComp.ChildNodes[0];
+                ApsimFile.Component childComp = thisComp.ChildNodes[0];
 
-               List<string> variableTypes = new List<string>();
-               LoadVariableTypes(Types.Instance.MetaDataNode("Factor", "FactorVariables"), variableTypes);
-               if(variableTypes.Contains(childComp.Type.ToLower()))
-               {
-                  Table = CreateTable(childComp);
-               }
+                List<string> variableTypes = new List<string>();
+                LoadVariableTypes(Types.Instance.MetaDataNode("Factor", "FactorVariables"), variableTypes);
+                if (variableTypes.Contains(childComp.Type.ToLower()))
+                {
+                    Table = CreateTable(childComp);
+                }
             }
             //gridManager.DataSourceTable = Table;
             gridManager.DataSource = Table;
+
+
 
             //Size the grid columns sensibly
             gridManager.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
@@ -130,57 +153,86 @@ namespace CSUserInterface
         public DataTable CreateTable(ApsimFile.Component childComp)
         {
             DataTable Table = new DataTable();
-            Table.Columns.Add(" ", typeof(System.Boolean));
-            Table.Columns.Add("names", typeof(string));
-            Table.Columns.Add("Manager Variables", typeof(string));
-            Table.Columns.Add("Parameters", typeof(string));
+
 
             XmlNodeList uiNodes = childComp.ContentsAsXML.SelectNodes("//ui/* | //CustomUI/*");
             bool isManager = true;
-           if(uiNodes.Count == 0)
+            if (uiNodes.Count == 0)
             {
-               uiNodes = childComp.ContentsAsXML.ChildNodes;
-               isManager = false;
+                uiNodes = childComp.ContentsAsXML.ChildNodes;
+                isManager = false;
             }
-            foreach (XmlNode ui in uiNodes)
-            {
-                DataRow newRow = Table.NewRow();
-                newRow[0] = false;
 
-                string varName = "";
-                string text = "";
-                if (isManager)
-                {
-                   varName = ui.Name;
-                   if (ui.Attributes["description"] != null)
-                      text = ui.Attributes["description"].Value;
-                   else
-                      text = varName;
-                }
+            bool useSingleColumnTable = uiNodes.Count == 1 || childComp.Type == "siloinput";
+
+            if (useSingleColumnTable)
+            {
+                csvLabel.Visible = false;
+
+                XmlNodeList foundNodes = Data.SelectNodes("//vars/" + uiNodes[0].Name);
+                string[] values = null;
+                if (foundNodes.Count == 1)
+                    values = foundNodes[0].InnerText.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                 else
-                {
-                   varName = XmlHelper.Name(ui);
-                }
-                newRow[1] = varName;
-                newRow[2] = text;
-                newRow[3] = ui.InnerText;
+                    values = new string[0];
+                DataTableUtility.AddColumn(Table, uiNodes[0].Name, values);
+                // If there is only 1 variable, then turn the table into lots of rows so that the user can
+                // enter one value per row rather than all on one line csv style.
+                for (int i = 0; i < 10000; i++)
+                    Table.Rows.Add(Table.NewRow());
 
-                //look for corresponding node in the variables node
-                if (varName != "#comment")
+            }
+            else
+            {
+                csvLabel.Visible = true;
+                Table.Columns.Add(" ", typeof(System.Boolean));
+                Table.Columns.Add("names", typeof(string));
+                Table.Columns.Add("Variables", typeof(string));
+                Table.Columns.Add("Parameters", typeof(string));
+                foreach (XmlNode ui in uiNodes)
                 {
-                    XmlNodeList foundNodes = Data.SelectNodes("//vars/" + varName);
-                    if (foundNodes.Count == 1)
+                    DataRow newRow = Table.NewRow();
+                    newRow[0] = false;
+
+                    string varName = "";
+                    string text = "";
+                    if (isManager)
                     {
-                        XmlNode varNode = foundNodes[0];
-                        if (varNode != null)
-                        {
-                            newRow[0] = true;
-                            newRow[3] = varNode.InnerText;
-                        }
+                        varName = ui.Name;
+                        if (ui.Attributes["description"] != null)
+                            text = ui.Attributes["description"].Value;
+                        else
+                            text = varName;
                     }
-                    Table.Rows.Add(newRow);
+                    else
+                    {
+                        varName = XmlHelper.Name(ui);
+                    }
+                    newRow[1] = varName;
+                    newRow[2] = text;
+                    newRow[3] = ui.InnerText;
+
+                    //look for corresponding node in the variables node
+                    if (varName != "#comment")
+                    {
+                        XmlNodeList foundNodes = Data.SelectNodes("//vars/" + varName);
+                        if (foundNodes.Count == 1)
+                        {
+                            XmlNode varNode = foundNodes[0];
+                            if (varNode != null)
+                            {
+                                newRow[0] = true;
+                                newRow[3] = varNode.InnerText;
+                            }
+                        }
+                        Table.Rows.Add(newRow);
+                    }
                 }
             }
+
+
+
+
             return Table;
         }
     }
