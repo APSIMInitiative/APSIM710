5c86ab4d233822e3a8acfe9280a4c08e9a4e7b5f
devoilp <p.devoil@uq.edu.au>
2014-04-03 11:14:59 +0000

    more /bin silliness bugid: 1377

--- Release/Release.sh
+++ Release/Release.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 dest=Temp
-mkdir $dest
-cp    $APSIM/Apsim.xml   $dest/Apsim.xml
-mkdir $dest/Model
-cp    $APSIM/Model/ApsimRun.sh $dest/Model
-cp    $APSIM/Model/*.xml $dest/Model
-cp    $APSIM/Model/*.so  $dest/Model
-cp    $APSIM/Model/*.dll $dest/Model
-if [ -f $dest/Model/APSIM.Tests.dll ]; then rm -f $dest/Model/APSIM.Tests.dll; fi
-cp    $APSIM/Model/*.exe $dest/Model
-cp    $APSIM/Model/*.x   $dest/Model
-mkdir $dest/Model/TclLink
-cp    $APSIM/Model/TclLink/CIDataTypes.tcl $dest/Model/TclLink
-mkdir $dest/UserInterface
-cp    $APSIM/UserInterface/*.xml $dest/UserInterface
-mkdir $dest/UserInterface/ToolBoxes
-cp    $APSIM/UserInterface/ToolBoxes/*.xml $dest/UserInterface/ToolBoxes
+/bin/mkdir $dest
+/bin/cp    $APSIM/Apsim.xml   $dest/Apsim.xml
+/bin/mkdir $dest/Model
+/bin/cp    $APSIM/Model/ApsimRun.sh $dest/Model
+/bin/cp    $APSIM/Model/*.xml $dest/Model
+/bin/cp    $APSIM/Model/*.so  $dest/Model
+/bin/cp    $APSIM/Model/*.dll $dest/Model
+if [ -f $dest/Model/APSIM.Tests.dll ]; then /bin/rm -f $dest/Model/APSIM.Tests.dll; fi
+/bin/cp    $APSIM/Model/*.exe $dest/Model
+/bin/cp    $APSIM/Model/*.x   $dest/Model
+/bin/mkdir $dest/Model/TclLink
+/bin/cp    $APSIM/Model/TclLink/CIDataTypes.tcl $dest/Model/TclLink
+/bin/mkdir $dest/UserInterface
+/bin/cp    $APSIM/UserInterface/*.xml $dest/UserInterface
+/bin/mkdir $dest/UserInterface/ToolBoxes
+/bin/cp    $APSIM/UserInterface/ToolBoxes/*.xml $dest/UserInterface/ToolBoxes
 
 if [ "$1" = "" ] ; then
   . $APSIM/Model/Build/VersionInfo.sh
-  if [ $(uname -m) == 'x86_64' ]; then
+  if [ $(/bin/uname -m) == 'x86_64' ]; then
      7zr a -sfx Apsim${MAJOR_VERSION}${MINOR_VERSION}-r${BUILD_NUMBER}.binaries.LINUX.X86_64.exe $dest
   else
      7zr a -sfx Apsim${MAJOR_VERSION}${MINOR_VERSION}-r${BUILD_NUMBER}.binaries.LINUX.INTEL.exe $dest
   fi
 else  
-  if [ $(uname -m) == 'x86_64' ]; then
+  if [ $(/bin/uname -m) == 'x86_64' ]; then
      7zr a -sfx $1.binaries.LINUX.X86_64.exe Temp
   else
      7zr a -sfx $1.binaries.LINUX.INTEL.exe Temp
   fi
 fi
 
-rm -rf Temp
+/bin/rm -rf Temp
