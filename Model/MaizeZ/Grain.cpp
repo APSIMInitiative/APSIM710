c32331155073d63c306ce4eff903467be63a57cf
zur003 <eric.zurcher@csiro.au>
2015-03-25 00:51:48 +0000

    Convert crop_chopped to BiomassRemoved in sorghum and maizez bugid: 2050

--- Model/MaizeZ/Grain.cpp
+++ Model/MaizeZ/Grain.cpp
@@ -308,47 +308,35 @@ void  Grain::Harvest(void)
    // send crop_chopped
    if(dmGreen > 0)
       {
-      // Build surface residues by part
-      vector<string> part_name;
-      vector<float> fraction_to_residue;           // fraction sent to residue (0-1)
-      vector<float> dlt_dm_crop;                   // change in dry matter of crop (kg/ha)
-      vector<float> dlt_dm_n;                      // N content of changed dry matter (kg/ha)
-      vector<float> dlt_dm_p;                      // P content of changed dry matter (kg/ha)
+      BiomassRemovedType chopped;
+      chopped.crop_type = plant->getCropType();
 
       double fracts[] = {0.0, 0.0, 0.0, 0.0, 0.0};  // No root or grain to residue.
 
+      // Build surface residues by part
       for (unsigned part = 0; part < plant->PlantParts.size(); part++)
          {
-         part_name.push_back(plant->PlantParts[part]->getName());
+         chopped.dm_type.push_back(plant->PlantParts[part]->getName());
          if(part < 4)
             {
-            dlt_dm_crop.push_back(0.0);
-            dlt_dm_n.push_back(0.0);
-            dlt_dm_p.push_back(0.0);
+            chopped.dlt_crop_dm.push_back(0.0);       // change in dry matter of crop (kg/ha)
+            chopped.dlt_dm_n.push_back(0.0);          // N content of changed dry matter (kg/ha)
+            chopped.dlt_dm_p.push_back(0.0);          // P content of changed dry matter (kg/ha)
             }
          else
             {
-            dlt_dm_crop.push_back((float)((plant->PlantParts[part]->getDmGreen() +
-               plant->PlantParts[part]->getDmSenesced()) * gm2kg/sm2ha));
-            dlt_dm_n.push_back((float)((plant->PlantParts[part]->getNGreen() +
-               plant->PlantParts[part]->getNSenesced()) * gm2kg/sm2ha));
-            dlt_dm_p.push_back((float)((plant->PlantParts[part]->getPGreen() +
-               plant->PlantParts[part]->getPSenesced()) * gm2kg/sm2ha));
+            chopped.dlt_crop_dm.push_back((float)((plant->PlantParts[part]->getDmGreen() +
+                  plant->PlantParts[part]->getDmSenesced()) * gm2kg/sm2ha));
+            chopped.dlt_dm_n.push_back((float)((plant->PlantParts[part]->getNGreen() +
+                  plant->PlantParts[part]->getNSenesced()) * gm2kg/sm2ha));
+            chopped.dlt_dm_p.push_back((float)((plant->PlantParts[part]->getPGreen() +
+                  plant->PlantParts[part]->getPSenesced()) * gm2kg/sm2ha));
             }
 
-         fraction_to_residue.push_back((float)fracts[part]);
+         chopped.fraction_to_residue.push_back((float)fracts[part]);
          }
 
-      Variant chopped;
-      chopped.pack("crop_type",   plant->getCropType());
-      chopped.pack("dm_type",     part_name);
-      chopped.pack("dlt_crop_dm", dlt_dm_crop);
-      chopped.pack("dlt_dm_n",    dlt_dm_n);
-      chopped.pack("dlt_dm_p",    dlt_dm_p);
-      chopped.pack("fraction_to_residue", fraction_to_residue);
-
-
-      scienceAPI.publish ("crop_chopped", chopped);
+      scienceAPI.publish("BiomassRemoved", chopped);
       }
    initialize();
    }
