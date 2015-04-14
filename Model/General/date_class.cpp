37c98f596696d62bac48ba4a0190bcb720d42291
zur003 <eric.zurcher@csiro.au>
2015-04-14 00:54:59 +0000

    Allow changes for use of GDate to work on Linux bugid: 2051

--- Model/General/date_class.cpp
+++ Model/General/date_class.cpp
@@ -1,4 +1,4 @@
-#include <..\General\pch.h>
+#include <../General/pch.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- Model/General/date_class.h
+++ Model/General/date_class.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <General/platform.h>
+#include <limits.h>
 
 // ------------------------------------------------------------------
 //  Short description:
