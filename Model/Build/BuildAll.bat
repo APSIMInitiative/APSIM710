9d09cd552e89908ad2c890c46fa56ca0e0fd401a
zur003 <eric.zurcher@csiro.au>
2015-07-24 11:49:37 +0000

    Allow Builds to be created with a range of different VS versions bugid: 3079

--- Model/Build/BuildAll.bat
+++ Model/Build/BuildAll.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 rem ----- Set the %APSIM% variable based on the directory where this batch file is located
 pushd %~dp0..\..
 set APSIM=%CD%
@@ -10,15 +11,41 @@ rem ----- This will tell the versionstamper not to increment the revision number
 rem ----- Change to model directory
 pushd %APSIM%\Model
 
-rem ----- Setup the Visual Studio 2010 compiler tools
-if "%LIBPATH%" == "" call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
+if NOT "%1" == "" (
+  SET VSTOOLPATH=VS%10COMNTOOLS
+  goto forceVersion
+)
+
+rem ----- Determine the highest installed version of Visual Studio on the current system
+rem ----- The set of numbers in the FOR statement indicate the VS version that we will look for
+rem ----- 10 = VS 2010
+rem ----- 11 = VS 2012
+rem ----- 12 = VS 2013
+rem ----- 14 = VS 2015
+rem ----- (There is no 13!)
+rem ----- If you want to force building with a specific version, just delete the numbers of the
+rem ----- versions you don't want to consider
+set VSTOOLPATH=""
+for %%a in (10 11 12 14) DO IF DEFINED VS%%a0COMNTOOLS (
+  SET VSTOOLPATH=VS%%a0COMNTOOLS
+)
+if "%VSTOOLPATH%" == "" (
+  echo Cannot locate the Visual Studio compiler!
+  popd
+  exit /B
+) 
+:forceVersion
+set VSTOOLPATH=!%VSTOOLPATH%!
+
+rem ----- Set up the Visual Studio compiler tools
+if "%LIBPATH%" == "" call "%VSTOOLPATH%..\..\VC\vcvarsall.bat"
 
 rem ----- Compile the job scheduler.
 echo Compiling the JobScheduler
-IF EXIST "%VS100COMNTOOLS%\..\IDE\devenv.exe" (
- "%VS100COMNTOOLS%\..\IDE\devenv.exe" "%APSIM%\Model\JobScheduler\JobScheduler.sln" /build debug
-) ELSE IF EXIST "%VS100COMNTOOLS%\..\IDE\vcsexpress.exe" (
- "%VS100COMNTOOLS%\..\IDE\vcsexpress.exe" "%APSIM%\Model\JobScheduler\JobScheduler.sln" /build debug
+IF EXIST "%VSTOOLPATH%\..\IDE\devenv.exe" (
+ "%VSTOOLPATH%\..\IDE\devenv.exe" "%APSIM%\Model\JobScheduler\JobScheduler.sln" /build debug
+) ELSE IF EXIST "%VSTOOLPATH%\..\IDE\vcsexpress.exe" (
+ "%VSTOOLPATH%\..\IDE\vcsexpress.exe" "%APSIM%\Model\JobScheduler\JobScheduler.sln" /build debug
 ) ELSE (
   echo "Cannot locate the Visual Studio compilers!"
   popd
