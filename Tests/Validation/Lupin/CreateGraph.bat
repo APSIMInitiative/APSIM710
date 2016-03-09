3844c53a6876cac5a74c8ef16e39f037e87c6248
hol353 <dean.holzworth@csiro.au>
2016-03-09 04:45:24 +0000

    New Lupin cultivar parameterisations bugid: 4121

--- Tests/Validation/Lupin/CreateGraph.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\ApsimReport\ApsimReport.exe" validation.report %APSIM%\Graphs\Lupin\Validation\LupinValidation.gif
-"C:\Program Files (x86)\ApsimReport\ApsimReport.exe" sensibility.report %APSIM%\Graphs\Lupin\Sensibility\LupinSensibility.gif
