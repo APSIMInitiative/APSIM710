5b6f7fd0a58602f17e368e808a679044b5388d9f
jonas.vejlin <jonas.vejlin>
2016-05-03 09:55:53 +0000

    Cow producing manure to slurrytank bugid: 1515

--- Model/cow/cow/cow.cs
+++ Model/cow/cow/cow.cs
@@ -3,43 +3,11 @@ using ModelFramework;
 
 public class cow
     {
-        
-        //gas
-        [Output]
-        [Units("kg")]
-        public double CCH4S =0;
-        [Output]
-        [Units("kg")]
-        public double CCO2_S=0;
-        [Output]
-        [Units("kg")]
-        public double CGas=0;
-        [Output]
-        [Units("kg")]
-        public double ENH3=0;
-        [Output]
-        [Units("kg")]
-        public double CH4EM = 0;
-        [Output]
-        [Units("kg")]
-        public double NN2O = 0;
-        [Output]
-        [Units("kg")]
-        public double NH3Emission = 0;
-        [Output]
-        [Units("kg")]
-        public double N2Emission = 0;
-        [Output]
-        [Units("kg")]
-        public double N2OEmission = 0;
-        [Output]
-        [Units("kg")]
-        public double CH4Emission = 0;
-        [Output]
-        [Units("kg")]
-        public double CO2Emission = 0;
 
-        [EventHandler]
+    [Event]
+    public event ManureDelegate AddSlurry;
+
+    [EventHandler]
         public void OnInitialised()
         {
   
@@ -50,62 +18,30 @@ public class cow
           
         }
 
-        [EventHandler]
-        public void OnCCH4SEvent(DoubleType CCH4S)
-        {
-            this.CCH4S += CCH4S.Value;
-        }
-        [EventHandler]
-        public void OnCCO2_SEvent(DoubleType CCO2_S)
-        {
-            this.CCO2_S += CCO2_S.Value;
-        }
-        [EventHandler]
-        public void OnCGasEvent(DoubleType CGas)
-        {
-            this.CGas += CGas.Value;
-        }
-        [EventHandler]
-        public void OnENH3Event(DoubleType ENH3)
-        {
-            this.ENH3 += ENH3.Value;
        
-        }
-        [EventHandler]
-        public void OnNN2OEvent(DoubleType NN2O)
-        {
-            this.NN2O += NN2O.Value;
-        }
-        [EventHandler]
-        public void OnCH4EMEvent(DoubleType CH4EM)
-        {
-            this.CH4EM += CH4EM.Value;
-        }
-        [EventHandler]
-        public void OnNH3EmissionEvent(DoubleType NH3Emission)
-        {
-            this.NH3Emission += NH3Emission.Value;
-        }
-        [EventHandler]
-        public void OnCN2OEmissionEvent(DoubleType N2OEmission)
-        {
-            this.N2OEmission += N2OEmission.Value;
-        }
-        [EventHandler]
-        public void OnCH4EmissionEvent(DoubleType CH4Emission)
-        {
-            this.CH4Emission += CH4Emission.Value;
-        }
-        [EventHandler]
-        public void OnCO2EmissionEvent(DoubleType CO2Emission)
-        {
-            this.CO2Emission += CO2Emission.Value;
-        }
         [EventHandler]
         public void OnProcess()
         {
-            
-        }
+        ManureType manure = new ManureType();
+
+        manure.amount = 1000.0; //kg
+        manure.Ash =   0.11; //proportion of DM
+        manure.Tan =  0.001; //proportion of FW
+        manure.RP =  0.188; //proportion of DM
+        manure.fInert =  0.0; //proportion of organic N in an inert form
+        manure.NDF =  0.271; //
+        manure.RL =  0.039;
+        manure.VFA =  0.0152;
+        manure.TotalS =  0.69;//g per kg fresh weight
+        manure.SulphS =  0.24; //g per kg fresh weight
+ 
+        manure.Rem = 1 - (manure.RL + manure.RP + manure.NDF + manure.VFA + manure.Ash);
+        manure.DM = 1;
+        manure.pH =7 ;
+
+        AddSlurry.Invoke( manure);
+           
+    }
 
     
 }
