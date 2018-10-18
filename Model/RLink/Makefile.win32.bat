14efbded322dad576892c001c7eef98cf95422eb
devoilp <p.devoil@uq.edu.au>
2018-10-18 03:15:05 +0000

    update R component to 3.5.1 bugid: 2061

--- Model/RLink/Makefile.win32.bat
+++ Model/RLink/Makefile.win32.bat
@@ -2,11 +2,13 @@
 @rem Requires RTools (from CRAN) to compile.
 @rem I've never managed to build this easily on windows. 
 
-set PATH=c:\Rtools\bin;c:\Rtools\gcc-4.6.3\bin;%windir%;%windir%\system32
+set PATH=c:\Rtools\bin;C:\Rtools\mingw_32\bin;%windir%;%windir%\system32
 set TMPDIR=c:/tmp
 
-g++ -c -IC:/R-3.2.0/include -IC:/R-3.2.0/library/Rcpp/include -IC:/R-3.2.0/library/RInside/include -Ic:/apsim/Model REmbed.cpp 
+g++ -c -IC:/R-3.5.1/include -IC:/R-3.5.1/library/Rcpp/include -IC:/R-3.5.1/library/RInside/include -Ic:/apsim/Model REmbed.cpp 
 
-g++ -c -IC:/R-3.2.0/include -IC:/R-3.2.0/library/Rcpp/include -IC:/R-3.2.0/library/RInside/include -Ic:/apsim/Model dllProcAddressWrapper.cpp
+g++ -c -IC:/R-3.5.1/include -IC:/R-3.5.1/library/Rcpp/include -IC:/R-3.5.1/library/RInside/include -Ic:/apsim/Model dllProcAddressWrapper.cpp
 
-gcc -shared -s -static-libgcc -o ../REmbed.dll REmbed.o dllProcAddressWrapper.o libRLink.a  "C:/R-3.2.0/bin/i386/R.dll" "C:/R-3.2.0/library/Rcpp/libs/i386/Rcpp.dll" "C:/R-3.2.0/library/RInside/lib/i386/libRInside.a" -lstdc++
+dlltool -d RLink.def -l libRLink.a
+
+gcc -shared -s -static-libgcc -o ../REmbed.dll REmbed.o dllProcAddressWrapper.o libRlink.a "C:/R-3.5.1/bin/i386/R.dll" "C:/R-3.5.1/library/Rcpp/libs/i386/Rcpp.dll" "C:/R-3.5.1/library/RInside/lib/i386/libRInside.a" -lstdc++
