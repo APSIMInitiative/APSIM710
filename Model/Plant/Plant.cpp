e424b4decb0854847e3345f0b611def39020c285
BenAbabaei <behnam.ab@gmail.com>
2019-12-10 10:54:52 +1000

    plant.doDmPotRUE() is called in Plant::onProcess(), as was originally.

--- Model/Plant/Plant.cpp
+++ Model/Plant/Plant.cpp
@@ -421,9 +421,10 @@ void Plant::onProcess()
 
 			// Calculate Potential Photosynthesis
 			// Ben Ababaei (2019.10.14): It's not needed if photosynthesis module calculates water demand internally.
-			if (!leaf().ExternalSWDemand) {
-				plant.doDmPotRUE();               // NIH - WHY IS THIS HERE!!!!?????  Not needed I hope.
-			}
+			//if (!leaf().ExternalSWDemand) {
+			//	plant.doDmPotRUE();               // NIH - WHY IS THIS HERE!!!!?????  Not needed I hope.
+			//}
+			plant.doDmPotRUE();               // NIH - WHY IS THIS HERE!!!!?????  Not needed I hope.
 
 			arbitrator().partitionDM();
 			arbitrator().doDmRetranslocate(&stem(), &leaf(), &fruit());
